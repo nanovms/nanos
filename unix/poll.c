d553cf093933418288a6d702eacee57d984831fa
Will Jhun <will@willjhun.com>
2018-09-10 15:34:56 -0400

    keep ref count for epollfd, for notifications may arrive after fd deletion

--- unix/poll.c
+++ unix/poll.c
@@ -1,6 +1,6 @@
 #include <unix_internal.h>
 
-#define EPOLL_DEBUG
+//#define EPOLL_DEBUG
 
 typedef struct epoll *epoll;
 
@@ -8,8 +8,10 @@ typedef struct epollfd {
     int fd; //debugging only
     file f;
     u64 data; // may be multiple versions of data?
+    u64 refcnt;
     epoll e;
     boolean registered;
+    boolean zombie;
     // xxx bind fd to first blocked that cares
 } *epollfd;
 
@@ -61,12 +63,12 @@ u64 epoll_create(u64 flags)
 
 #define user_event_count(__w) (buffer_length(__w->user_events)/sizeof(struct epoll_event))
 
-static void epoll_blocked_remove(epoll_blocked w)
+static void epoll_blocked_release(epoll_blocked w)
 {
     epoll e = w->e;
     kernel k = current->p->k;
 #ifdef EPOLL_DEBUG
-    rprintf("epoll_blocked_remove: w %p", w);
+    rprintf("epoll_blocked_release: w %p", w);
 #endif
     if (!list_empty(&w->blocked_list)) {
 	list_delete(&w->blocked_list);
@@ -93,7 +95,7 @@ static void epoll_blocked_finish(epoll_blocked w)
     if (w->sleeping)
 	rprintf(", sleeping");
     if (w->timeout)
-	rprintf(", timeout %p", w->timeout);
+	rprintf(", timeout %p\n", w->timeout);
 #endif
     kernel k = current->p->k;
     heap h = k->general;
@@ -103,7 +105,7 @@ static void epoll_blocked_finish(epoll_blocked w)
        notifications, if any, to be applied. */
     if (w->sleeping) {
 #ifdef EPOLL_DEBUG
-	rprintf(", syscall return %d", user_event_count(w));
+	rprintf("   syscall return %d\n", user_event_count(w));
 #endif
         set_syscall_return(w->t, user_event_count(w));
         w->sleeping = false;
@@ -122,15 +124,23 @@ static void epoll_blocked_finish(epoll_blocked w)
 	    */
 	    w->refcnt++;
 	}
-	epoll_blocked_remove(w);
+	epoll_blocked_release(w);
     } else if (w->timeout) {
 	/* expiry after syscall return */
 	assert(w->refcnt == 1);
-	epoll_blocked_remove(w);
+	epoll_blocked_release(w);
     }
+}
+
+static void epollfd_release(epollfd f)
+{
+    kernel k = current->p->k;
 #ifdef EPOLL_DEBUG
-    rprintf("\n");
+    rprintf("epollfd_release: f->fd %d, refcnt %d\n", f->fd, f->refcnt);
 #endif
+    assert(f->refcnt > 0);
+    if (--f->refcnt == 0)
+	deallocate(k->epollfd_cache, f, sizeof(struct epollfd));
 }
 
 // associated with the current blocking function
@@ -140,22 +150,24 @@ static void epoll_wait_notify(epollfd f, u32 events)
     list l = list_get_next(&f->e->blocked_head);
     epoll_blocked w = l ? struct_from_list(l, epoll_blocked, blocked_list) : 0;
 #ifdef EPOLL_DEBUG
-    rprintf("epoll_wait_notify: f->fd %d, events %P, blocked %p\n", f->fd, events, w);
+    rprintf("epoll_wait_notify: f->fd %d, events %P, blocked %p, zombie %d\n",
+	    f->fd, events, w, f->zombie);
 #endif
     f->registered = false;
-    if (!w)
-	return;
-    // strided vectors?
-    if (w->user_events && (w->user_events->length - w->user_events->end)) {
-	struct epoll_event *e = buffer_ref(w->user_events, w->user_events->end);
-	e->data = f->data;
-	e->events = events;
-	w->user_events->end += sizeof(struct epoll_event);
+    if (!f->zombie && w) {
+	// strided vectors?
+	if (w->user_events && (w->user_events->length - w->user_events->end)) {
+	    struct epoll_event *e = buffer_ref(w->user_events, w->user_events->end);
+	    e->data = f->data;
+	    e->events = events;
+	    w->user_events->end += sizeof(struct epoll_event);
 #ifdef EPOLL_DEBUG
-	rprintf("   epoll_event %p, data %P, events %P\n", e, e->data, e->events);
+	    rprintf("   epoll_event %p, data %P, events %P\n", e, e->data, e->events);
 #endif
+	}
+	epoll_blocked_finish(w);
     }
-    epoll_blocked_finish(w);
+    epollfd_release(f);
 }
 
 int epoll_wait(int epfd,
@@ -185,6 +197,7 @@ int epoll_wait(int epfd,
         epollfd f = (epollfd)i;
         if (!f->registered) {
             f->registered = true;
+	    f->refcnt++;
 #ifdef EPOLL_DEBUG
 	    rprintf("   register epollfd %d, applying check\n", f->fd);
 #endif
@@ -198,7 +211,7 @@ int epoll_wait(int epfd,
 #ifdef EPOLL_DEBUG
 	rprintf("   immediate return; eventcount %d\n", eventcount);
 #endif
-	epoll_blocked_remove(w);
+	epoll_blocked_release(w);
         return eventcount;
     }
     
@@ -221,6 +234,9 @@ u64 epoll_ctl(int epfd, int op, int fd, struct epoll_event *event)
     kernel k = current->p->k;
     value ekey = pointer_from_u64((u64)fd);
     epoll e = resolve_fd(current->p, epfd);    
+#ifdef EPOLL_DEBUG
+    rprintf("epoll_ctl: epoll fd %d, op %d, fd %d\n", epfd, op, fd);
+#endif
     switch(op) {
     case EPOLL_CTL_ADD:
         {
@@ -230,8 +246,13 @@ u64 epoll_ctl(int epfd, int op, int fd, struct epoll_event *event)
             f->fd = fd;
             f->e = e;
             f->data = event->data;
-            f->registered = 0;
+	    f->refcnt = 1;
+            f->registered = false;
+	    f->zombie = false;
             table_set(e->events, ekey, f);
+#ifdef EPOLL_DEBUG
+	    rprintf("   added %d, epollfd %p\n", fd, f);
+#endif
         }
         break;
 
@@ -243,11 +264,17 @@ u64 epoll_ctl(int epfd, int op, int fd, struct epoll_event *event)
     case EPOLL_CTL_DEL:
         {
 	    epollfd f = table_find(e->events, ekey);
-	    if (f)
-		deallocate(k->epollfd_cache, f, sizeof(struct epollfd));
-	    else
+	    if (!f) {
 		msg_err("epollfd not found for fd %d\n", fd);
+		return -EBADF;
+	    }
 	    table_set(e->events, ekey, 0);
+	    assert(f->refcnt > 0);
+	    f->zombie = true;
+	    epollfd_release(f);
+#ifdef EPOLL_DEBUG
+	    rprintf("   removed %d, epollfd %p, refcnt %d\n", fd, f, f->refcnt);
+#endif
 	}
     }
     return 0;
