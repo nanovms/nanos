592a4fa1fe66daeef37f237e6849219c1465fd97
Will Jhun <will@willjhun.com>
2018-09-28 16:17:35 -0400

    format output for halt

--- x86_64/kvm_platform.c
+++ x86_64/kvm_platform.c
@@ -2,9 +2,18 @@
 #include <kvm_platform.h>
 
 // the handler for this in qemu overwrites the exit code to insure its nonzero
-void halt(char *x, ...)
+void halt(char *format, ...)
 {
-    console(x);
+    vlist a;
+    buffer b = little_stack_buffer(512);
+    struct buffer f;
+    f.start = 0;
+    f.contents = format;
+    f.end = runtime_strlen(format);
+
+    vstart(a, format);
+    vbprintf(b, &f, &a);
+    debug(b);
     QEMU_HALT();
 }
 
