06e4ba50ed5cb9c4b1ecf6cac8d8a55834fbb636
yuri <yuri@eclair.gateway.sonic.net>
2018-10-08 09:08:18 -0700

    dep amke

---
+++ dep.mk
@@ -0,0 +1,16 @@
+DEPDIR := .d
+$(shell mkdir -p $(DEPDIR) >/dev/null)
+DEPFLAGS = -MT $@ -MMD -MP -MF $(DEPDIR)/$*.Td
+
+COMPILE.c = $(CC) $(DEPFLAGS) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c
+POSTCOMPILE = @mv -f $(DEPDIR)/$*.Td $(DEPDIR)/$*.d && touch $@
+
+$(DEPDIR)/%.d : %.c $(DEPDIR)/%.d
+	$(COMPILE.c) $<
+	$(POSTCOMPILE)
+
+$(DEPDIR)/%.d: ;
+.PRECIOUS: $(DEPDIR)/%.d
+
+include $(wildcard $(patsubst %,$(DEPDIR)/%.d,$(basename $(SRCS))))
+
