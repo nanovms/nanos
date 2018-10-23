b48ed5a1aa475c4f870ba9f6adf3a111a57ef1c4
Tzu-Jung Lee <roylee17@gmail.com>
2018-10-22 19:34:28 -0700

    unix: add mktime

---
+++ unix/mktime.c
@@ -0,0 +1,45 @@
+#include <runtime.h>
+#include <io.h>
+#include <mktime.h>
+
+static int days_in_month(int m) {
+    if (m == 2) {
+        return 28;
+    }
+    if (m == 4 || m == 6 || m == 9 || m == 11) {
+        return 30;
+    }
+    return 31;
+}
+
+static int is_leap_year(int year) {
+    if ((year & 3) != 0) {
+        return 0;
+    }
+    if ((year % 100) != 0) {
+        return 1;
+    }
+    return (year % 400) == 0;
+}
+
+int mktime(struct tm *tm) {
+    int days = 365 * (tm->tm_year - 1970);
+
+    for (int i = 1970; i < tm->tm_year; i++) {
+        if (is_leap_year(i)) {
+            days++;
+        }
+    }
+
+    if (is_leap_year(tm->tm_year) && tm->tm_mon > 2) {
+        days++;
+    }
+
+    for (int i = 1; i < tm->tm_mon; i++) {
+        days += days_in_month(i);
+    }
+
+    days += (tm->tm_mday - 1);
+
+    return ((days * 24 + tm->tm_hour) * 60 + tm->tm_min) * 60 + tm->tm_sec;
+}
---
+++ unix/mktime.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <runtime.h>
+
+struct tm {
+    int tm_year;
+    u8 tm_mon;
+    u8 tm_mday;
+    u8 tm_hour;
+    u8 tm_min;
+    u8 tm_sec;
+};
+
+extern int mktime(struct tm *tm);
