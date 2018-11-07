2ce7258501427e1d4b7aecbf41d8c118efbc8a16
Levente Kurusa <lkurusa@acm.org>
2018-11-07 08:10:04 +0100

    examples: write: seek to the beginning
    Signed-off-by: Levente Kurusa <lkurusa@acm.org>

--- examples/write.c
+++ examples/write.c
@@ -11,9 +11,17 @@ main(int argc, char **argv)
 {
     int fd = open("/hello", O_CREAT, O_RDWR);
     read(fd, buf, 256);
-    printf("original: \"%s\"\n", buf);
-    write(fd, str, strlen(str));
-    close(fd);
 
+    printf("original: \"%s\"\n", buf);
+    lseek(fd, 0, SEEK_SET);
+    write(fd, str, strlen(str));
+    lseek(fd, 0, SEEK_SET);
+
+    memset(buf, 0, 256);
+
+    read(fd, buf, 256);
+    printf("new: \"%s\"\n", buf);
+
+    close(fd);
     return 0;
 }
