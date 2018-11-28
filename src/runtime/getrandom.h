6e463f9a4ff0264626e0f0f76079fa7f4817c94a
Levente Kurusa <lkurusa@acm.org>
2018-11-28 10:09:14 +0100

    Cleanup getrandom(2)
    Signed-off-by: Levente Kurusa <lkurusa@acm.org>

--- src/runtime/getrandom.h
+++ src/runtime/getrandom.h
@@ -1,9 +1,8 @@
-//definitions as per man page of getrandom(2)
-#define MAX_ENTROPY_POOL 4096
-#define GRND_NONBLOCK 1
-#define GRND_RANDOM 2
-#define MAX_RANDOM_ENTROPY_COUNT 256
+#pragma once
 
-int rand (void);
-int getrandom(void *buf,int bufLength,unsigned int flags);
+#define MAX_ENTROPY_POOL            4096
+#define GRND_NONBLOCK               1
+#define GRND_RANDOM                 2
+#define MAX_RANDOM_ENTROPY_COUNT    256
 
+u64 do_getrandom(buffer b, u64 flags);
