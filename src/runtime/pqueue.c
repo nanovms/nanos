7344003a3adb563dd4b167ab1290ef897d790e64
Will Jhun <will@willjhun.com>
2018-11-28 02:02:17 -0500

    wrong arg order for qcompare, peek doesn't return boolean

--- src/runtime/pqueue.c
+++ src/runtime/pqueue.c
@@ -14,15 +14,14 @@ static inline void swap(pqueue q, index x, index y)
     vector_set(q->body, y-1, temp);
 }
 
-
 #define qcompare(__q, __x, __y)\
   (q->sort(vector_get((__q)->body, (__x-1)), \
                 vector_get((__q)->body, (__y-1))))
 
 static void heal(pqueue q, index where)
 {
-    index left= where<<1;
-    index right = left+1;
+    index left = where << 1;
+    index right = left + 1;
     index min = where;
     index len = vector_length(q->body);
     
@@ -42,12 +41,11 @@ static void add_pqueue(pqueue q, index i)
 {
     index parent = i >> 1;
 
-    if ((parent > 0) && qcompare(q, i, parent)) {
+    if ((parent > 0) && qcompare(q, parent, i)) {
         swap(q, i, parent);
         add_pqueue(q, parent);
     }
 }
-
 
 void pqueue_insert(pqueue q, void *v)
 {
@@ -73,7 +71,7 @@ void *pqueue_pop(pqueue q)
 void *pqueue_peek(pqueue q)
 {
     if (vector_length(q->body)) return(vector_get(q->body, 0));
-    return(false);
+    return 0; // XXX INVALID_ADDRESS?
 }
 
 pqueue allocate_pqueue(heap h, boolean(*sort)(void *, void *))
