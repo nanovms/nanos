236ec84f48cbc0d3ed8f2bad86c7c667f3a2c0f1
crodnun <crodnun@yahoo.es>
2018-12-17 19:42:44 +0100

    Warning fixed under OSX/clang (it should be fixed everywhere!!), return something when conditions are not met

--- src/runtime/rtrie.c
+++ src/runtime/rtrie.c
@@ -200,12 +200,20 @@ static u64 rtrie_extent_max(rtnode r)
     u64 k;
     if (!r) return 0;
     if (!(k = rtrie_extent_max(r->children[1]))) return (r->r.end);
+#ifdef __APPLE__
+    // Return something by default to avoid warning
+    return 0;
+#endif
 }
  
 static u64 rtrie_extent_min(rtnode r)
 {
     if (!r) return 0;
     if (!rtrie_extent_min(r->children[0])) return (r->r.start);
+#ifdef __APPLE__
+    // Return something by default to avoid warning
+    return 0;
+#endif
 }
 
 void rtrie_extent(rtrie r, u64 *min, u64 *max)
