011eb61869baf0c0977c6946cbdcac092ce42a3b
crodnun <crodnun@yahoo.es>
2018-12-18 20:51:07 +0100

    We have a mess with many now() functions and sys_now() function, we should use the timer.h module for the same, sys_now is commented out here, not used along the project

--- src/net/lwipopts.h
+++ src/net/lwipopts.h
@@ -101,14 +101,14 @@ static inline void sys_arch_unprotect(sys_prot_t x)
 
 extern void console(char *);
 typedef unsigned long long time; 
-extern timestamp now();
+//extern timestamp now();
 extern void lwip_debug(char * format, ...);
 
-static inline u32_t sys_now(void)
-{
-    timestamp t = now();
-    return (t * 1000) >> 32;
-}
+//static inline u32_t sys_now(void)
+//{
+//    timestamp t = now();
+//    return (t * 1000) >> 32;
+//}
 
 #define MEM_LIBC_MALLOC 1
 
