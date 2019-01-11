0a3ecdd0a06662e5b0940a5d2dd69e898ed420fc
Max Khon <fjoe@samodelkin.net>
2019-01-12 04:18:54 +0700

    Remove duplicate #definitions of CLONE_* flags.

--- src/unix/unix_internal.h
+++ src/unix/unix_internal.h
@@ -14,9 +14,6 @@ typedef s64 sysreturn;
 typedef struct process *process;
 typedef struct thread *thread;
 
-#define CLONE_PARENT_SETTID     0x00100000      /* set the TID in the parent */
-#define CLONE_CHILD_CLEARTID    0x00200000      /* clear the TID in the child */
-
 thread create_thread(process);
 void exit_thread(thread);
 
