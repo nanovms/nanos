be9000474c7329eea7aade94450986986ca2ebe9
Max Khon <fjoe@samodelkin.net>
2019-02-02 03:45:39 +0700

    - exit_group: Add missing \n - Remove unused haltf() - halt() is marked as "noreturn"

--- src/x86_64/kvm_platform.h
+++ src/x86_64/kvm_platform.h
@@ -3,16 +3,10 @@
 #include <disk.h>
 #include <booto.h>
 
-#define QEMU_HALT()  out8(0x501, 0);
+static inline void QEMU_HALT() __attribute__((noreturn));
 
-static inline void haltf(char *f, ...)
+static inline void QEMU_HALT()
 {
-    buffer bf = alloca_wrap_buffer(f, runtime_strlen(f));
-    buffer b = little_stack_buffer(2048);
-    vlist ap;
-    vstart (ap, f);
-    vbprintf(b, bf,  &ap);
-    debug(b->contents);
-    QEMU_HALT();
+    out8(0x501, 0); // does not return
+    while (1);
 }
-
