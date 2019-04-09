742134373b345c2d3ccccbf738d58d7757327468
Max Khon <max@nanovms.com>
2019-04-10 03:36:35 +0700

    Add null page runtime test.

---
+++ test/runtime/nullpage.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int
+main()
+{
+	setbuf(stdout, NULL);
+	printf("started\n");
+
+	char *p = 0;
+	memcpy(p, "\xcd\x03", 2);
+
+	printf("before\n");
+	((void(*)())p)();
+	printf("after\n");
+
+	return 0;
+}
