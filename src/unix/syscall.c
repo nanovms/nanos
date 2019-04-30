393614d2df343fc8349c006dad1f5f2afbe49c19
Max Khon <max@nanovms.com>
2019-04-30 14:57:03 +0700

    Enable back features required for JRE8: - sysinfo(2) - /sys/devices/system/cpu/cpu0

--- src/unix/syscall.c
+++ src/unix/syscall.c
@@ -1237,8 +1237,6 @@ sysreturn prctl(int option, u64 arg2, u64 arg3, u64 arg4, u64 arg5)
 
 sysreturn sysinfo(struct sysinfo *info)
 {
-    return set_syscall_error(current, ENOSYS);
-
     if (info == 0)
         return set_syscall_error(current, EINVAL);
 
