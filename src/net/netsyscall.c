nanovms/nanos

commit b900ee4f8525759aebabf93180cec600a13df1f0

socket read: fix handling of chained pbuf structures
When a network packet is received on a socket, it may be delivered
by lwIP as a chain of struct pbuf elements, where the link from an
element to the next is given by the "next" member of struct pbuf.
The sock_read_bh() function was handling only the first pbuf
structure of a chain, thereby discarding the rest of data in
packets with more than one chain element.
This commit fixes the above issue, and in addition fixes another
issue in sock_read_bh() where packets received from a datagram
socket whose data does not fit in the reception buffer were not
deallocated after a partial read: the correct behavior for a
partially read datagram is to discard the data that could not fit
in the reception buffer.

Closes #881

src/net/netsyscall.c
@@ -328,16 +328,24 @@ static sysreturn sock_read_bh(sock s, thread t, void *dest, u64 length,
     do {
         struct pbuf * pbuf = s->type == SOCK_STREAM ? (struct pbuf *)p :
             ((struct udp_entry *)p)->pbuf;
-        assert(pbuf->len > 0);
+        struct pbuf *cur_buf = pbuf;
 
-        u64 xfer = MIN(length, pbuf->len);
-        runtime_memcpy(dest, pbuf->payload, xfer);
-        pbuf_consume(pbuf, xfer);
-        length -= xfer;
-        xfer_total += xfer;
-        dest = (char *) dest + xfer;
+        do {
+            if (cur_buf->len == 0) {
+                cur_buf = cur_buf->next;
+                continue;
+            }
+            u64 xfer = MIN(length, cur_buf->len);
+            runtime_memcpy(dest, cur_buf->payload, xfer);
+            pbuf_consume(cur_buf, xfer);
+            length -= xfer;
+            xfer_total += xfer;
+            dest = (char *) dest + xfer;
+            if (s->type == SOCK_STREAM)
+                tcp_recved(s->info.tcp.lw, xfer);
+        } while ((length > 0) && cur_buf);
 
-        if (pbuf->len == 0) {
+        if (!cur_buf || (s->type == SOCK_DGRAM)) {
             assert(dequeue(s->incoming) == p);
             if (s->type == SOCK_DGRAM)
                 deallocate(s->h, p, sizeof(struct udp_entry));
@@ -346,9 +354,6 @@ static sysreturn sock_read_bh(sock s, thread t, void *dest, u64 length,
             if (!p)
                 notify_sock(s); /* reset a triggered EPOLLIN condition */
         }
-
-        if (s->type == SOCK_STREAM)
-            tcp_recved(s->info.tcp.lw, xfer);
     } while(s->type == SOCK_STREAM && length > 0 && p); /* XXX simplify expression */
 
     rv = xfer_total;

commit 436598907816ac190d1aafcb440570df7ca04f21

sock_read_bh(): fix behavior when input data size == buffer size
This fixes a regression introduced in commit
b900ee4f8525759aebabf93180cec600a13df1f0 (pull request #887).
When the size of available input data for a socket coincides with
the size of the reception buffer passed to the read() routine,
the packet buffer structure containing the input data must be
removed from the input queue and deallocated, since its contents
have been completely consumed; otherwise, a zero-length packet
buffer remains in the input queue after the call to the read()
routine, and this causes a subsequent call to read() to return 0.

Closes #891.

src/net/netsyscall.c
@@ -331,18 +331,18 @@ static sysreturn sock_read_bh(sock s, thread t, void *dest, u64 length,
         struct pbuf *cur_buf = pbuf;
 
         do {
-            if (cur_buf->len == 0) {
-                cur_buf = cur_buf->next;
-                continue;
+            if (cur_buf->len > 0) {
+                u64 xfer = MIN(length, cur_buf->len);
+                runtime_memcpy(dest, cur_buf->payload, xfer);
+                pbuf_consume(cur_buf, xfer);
+                length -= xfer;
+                xfer_total += xfer;
+                dest = (char *) dest + xfer;
+                if (s->type == SOCK_STREAM)
+                    tcp_recved(s->info.tcp.lw, xfer);
             }
-            u64 xfer = MIN(length, cur_buf->len);
-            runtime_memcpy(dest, cur_buf->payload, xfer);
-            pbuf_consume(cur_buf, xfer);
-            length -= xfer;
-            xfer_total += xfer;
-            dest = (char *) dest + xfer;
-            if (s->type == SOCK_STREAM)
-                tcp_recved(s->info.tcp.lw, xfer);
+            if (cur_buf->len == 0)
+                cur_buf = cur_buf->next;
         } while ((length > 0) && cur_buf);
 
         if (!cur_buf || (s->type == SOCK_DGRAM)) {