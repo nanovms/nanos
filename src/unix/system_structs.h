75a08eb9207722a6d08a47d18c92c701a08e9798
Francesco Lavra <francescolavra.fl@gmail.com>
2019-07-23 17:33:34 +0200

    clock_gettime(2): properly handle monotonic clock
    The now() function returns the wall clock time, so it will be
    subject to discontinuities if the system time is changed (when
    support for changing the system time is actually implemented), and
    for this reason it should not be used as time source in
    clock_gettime() in case the requested clock is a monotonic clock.
    This commit uses uptime() as time source in such cases.
    In addition, the CLOCK_PROCESS_CPUTIME_ID clock type is now
    implemented as well.
    
    Closes #357.

--- src/unix/system_structs.h
+++ src/unix/system_structs.h
@@ -168,6 +168,14 @@ typedef struct iovec {
 
 typedef int clockid_t;
 
+#define CLOCK_REALTIME              0
+#define CLOCK_MONOTONIC             1
+#define CLOCK_PROCESS_CPUTIME_ID    2
+#define CLOCK_THREAD_CPUTIME_ID     3
+#define CLOCK_MONOTONIC_RAW         4
+#define CLOCK_REALTIME_COARSE       5
+#define CLOCK_MONOTONIC_COARSE      6
+
 struct timespec {
 	u64 ts_sec;
 	u64 ts_nsec;
