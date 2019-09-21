fbcc47adc0c889f016e3f7e5ddcd4d35e16d9729
Brian Kocoloski <brian.kocoloski@wustl.edu>
2019-09-21 01:18:38 -0500

    Revert "Reimplement futex with based on the new blockq implementation."
    This reverts commit 66d2bc10a9dca69517c5d8b4dd220e97f2a5d09a.

--- src/unix/thread.c
+++ src/unix/thread.c
@@ -2,11 +2,6 @@
 
 thread current;
 
-struct futex {
-    heap h;
-    blockq bq;
-};
-
 CLOSURE_1_1(default_fault_handler, context, thread, context);
 
 static u64 futex_key_function(void *x)
@@ -91,256 +86,177 @@ sysreturn clone(unsigned long flags, void *child_stack, int *ptid, int *ctid, un
     return t->tid;
 }
 
-static struct futex * soft_create_futex(process p, u64 key)
+typedef struct fut {
+    queue waiters;
+    timer t;
+} *fut;
+    
+static fut soft_create_futex(process p, u64 key)
 {
+    fut f;
     heap h = heap_general(get_kernel_heaps());
-    struct futex * f;
-
-    // XXX make this all atomic wrt p->futices
-
-    f = table_find(p->futices, pointer_from_u64(key));
-    if (f)
-        return f;
-
-    f = allocate(h, sizeof(struct futex));
-    if (!f) {
-        msg_err("failed to allocate futex\n");
-        return INVALID_ADDRESS;
+    // of course this is supossed to be serialized
+    if (!(f = table_find(p->futices, pointer_from_u64(key)))) {
+        f = allocate(h, sizeof(struct fut));
+        f->waiters = allocate_queue(h, 32);
+        f->t = 0;
+        table_set(p->futices, pointer_from_u64(key), f);
     }
-
-    f->h = h;
-    f->bq = allocate_blockq(f->h, "futex");
-    if (f->bq == INVALID_ADDRESS) {
-        msg_err("failed to allocate futex blockq\n");
-        deallocate(f->h, f, sizeof(struct futex));
-        return INVALID_ADDRESS;
-    }
-
-    table_set(p->futices, pointer_from_u64(key), f);
     return f;
 }
 
-static thread futex_wake_one(struct futex * f)
+static void futex_thread_wakeup(fut f, thread t) {
+    if (f->t){
+        remove_timer(f->t);
+    }
+    thread_wakeup(t);
+}
+
+// return the number of waiters that were woken up
+static int futex_wake(fut f, int val, boolean verbose, int *uaddr)
 {
+    int result = 0;
     thread w;
 
-    w = blockq_wake_one(f->bq);
-    if (w == INVALID_ADDRESS)
-        return w;
-
-    /* w must be awake */
-    assert(w->blocked_on != f->bq);
-    return w;
-}
-
-/*
- * Wake up to 'val' waiters
- * Return the number woken
- */
-static int futex_wake_many(struct futex * f, int val)
-{
-    int nr_woken;
-
-    for (nr_woken = 0; nr_woken < val; nr_woken++) {
-        thread w = futex_wake_one(f);
-        if (w == INVALID_ADDRESS)
-            break;
+    while (result < val && (w = dequeue(f->waiters))) {
+        result++;
+        if (verbose) {
+            thread_log(current, "futex_wake [%ld %p %d] %p %d/%d",
+                current->tid, uaddr, *uaddr, w, result, val);
+        }
+        futex_thread_wakeup(f, w);
     }
 
-    return nr_woken;
+    return result;
 }
 
-/*
- * futex_bh is invoked either by the bh processor in response
- * to timeout/signal delivery/etc., or by another thread in sys_futex
- *
- * Return:
- *  infinity: timer still active
- *  -ETIMEDOUT: if we timed out
- *  -EINTR: if we're being nullified
- *  0: thread woken up
- */
-static CLOSURE_3_2(futex_bh, sysreturn, struct futex *, thread, timestamp,
-                   boolean, boolean);
-static sysreturn futex_bh(struct futex * f, thread t, timestamp ts,
-                          boolean blocked, boolean nullify)
+static CLOSURE_1_0(futex_timeout, void, thread);
+static void futex_timeout(thread t)
 {
-    sysreturn rv;
-
-    if (current == t)
-        rv = infinity;
-    else if (nullify)
-        rv = -EINTR;
-    else if (ts > 0 && ts < now()) /* has timer expired */
-        rv = -ETIMEDOUT;
-    else
-        rv = 0; /* no timer expire + not us --> actual wakeup */
-
-    if (rv != infinity)
-        thread_wakeup(t);
-
-    return set_syscall_return(t, rv);
+    set_syscall_return(t, ETIMEDOUT);
+    thread_wakeup(t);
 }
 
-static timestamp get_timeout_timestamp(int futex_op, u64 val2)
+void register_futex_timer(thread t, fut f, const struct timespec* req)
 {
-    switch (futex_op) {
-    case FUTEX_WAIT:
-    case FUTEX_CMP_REQUEUE:
-    case FUTEX_WAIT_BITSET:
-        return (val2) 
-            ? time_from_timespec((struct timespec *)pointer_from_u64(val2)) 
-            : 0;
-    default:
-        return 0;
-    }
+   f->t = register_timer(time_from_timespec(req),
+		closure(heap_general(get_kernel_heaps()), futex_timeout, t));
 }
 
 static sysreturn futex(int *uaddr, int futex_op, int val,
                        u64 val2,
                        int *uaddr2, int val3)
 {
-    struct futex * f;
-    timestamp ts;
-    int op;
-
-    boolean verbose = table_find(current->p->process_root, sym(futex_trace))
-        ? true : false;
-
-    f = soft_create_futex(current->p, u64_from_pointer(uaddr));
-    if (f == INVALID_ADDRESS)
-        return set_syscall_error(current, ENOMEM);
+    struct timespec *timeout = pointer_from_u64(val2);
+    boolean verbose = table_find(current->p->process_root, sym(futex_trace))?true:false;
+    thread w;
     
-    op = futex_op & 127; // chuck the private bit
-    ts = get_timeout_timestamp(op, val2);
-
-    switch (op) {
-    case FUTEX_WAIT: {
-        if (verbose)
-            thread_log(current, "futex_wait [%ld %p %d] %d 0x%ld",
-                current->tid, uaddr, *uaddr, val, val2);
-
-        if (*uaddr != val)
-            return set_syscall_error(current, EAGAIN);
-
-        // if we resume we are woken up
-        set_syscall_return(current, 0);
-
-        return blockq_check_timeout(f->bq, current, 
-            closure(f->h, futex_bh, f, current, ts),
-            false, ts
-        );
-    }
+    fut f = soft_create_futex(current->p, u64_from_pointer(uaddr));
+    int op = futex_op & 127; // chuck the private bit
+    switch(op) {
+    case FUTEX_WAIT:
+        if (*uaddr == val) {
+            if (verbose) {
+                thread_log(current, "futex_wait [%ld %p %d] %d %p",
+                    current->tid, uaddr, *uaddr, val, timeout);
+            }
+            // if we resume we are woken up
+            set_syscall_return(current, 0);
+            //timeout is relative, measured against the CLOCK_MONOTONIC clock
+            if (timeout)
+                register_futex_timer(current, f, timeout);
+            // atomic 
+            enqueue(f->waiters, current);
+            thread_sleep_uninterruptible(); /* XXX move to blockq */
+        }
+        return -EAGAIN;
             
-    case FUTEX_WAKE: {
-        if (verbose)
-            thread_log(current, "futex_wake [%ld %p %d] %d",
-                current->tid, uaddr, *uaddr, val);
-        return set_syscall_return(current, futex_wake_many(f, val));
-    }
+    case FUTEX_WAKE:
+        return futex_wake(f, val, verbose, uaddr);
         
-    case FUTEX_CMP_REQUEUE: {
-        int wake1, wake2;
-        struct futex * f2;
-        thread w;
-
-        if (verbose)
+    case FUTEX_REQUEUE: rprintf("futex_requeue not implemented\n"); break;
+    case FUTEX_CMP_REQUEUE:
+        if (verbose) {
             thread_log(current, "futex_cmp_requeue [%ld %p %d] %d %p %d",
                 current->tid, uaddr, *uaddr, val3, uaddr2, *uaddr2);
+        }
+        if (*uaddr == val3) {
+            int result = futex_wake(f, val, verbose, uaddr);
 
-        if (*uaddr != val3)
-            return set_syscall_error(current, EAGAIN);
-
-        wake1 = futex_wake_many(f, val);
-        for (wake2 = 0, f2 = INVALID_ADDRESS; wake2 < val2; wake2++) {
-            w = futex_wake_one(f);
-            if (w == INVALID_ADDRESS)
-                break;
-
-            if (f2 == INVALID_ADDRESS) {
-                f2 = soft_create_futex(current->p, u64_from_pointer(uaddr2));
-                if (f2 == INVALID_ADDRESS)
-                    return set_syscall_error(current, ENOMEM);
+            int result2 = 0;
+            if (queue_peek(f->waiters)) {
+                fut f2 = soft_create_futex(current->p, u64_from_pointer(uaddr2));
+                while (result2 < val2 && (w = dequeue(f->waiters))) {
+                    result2++;
+                    if (verbose) {
+                        thread_log(current, "futex_cmp_requeue [%ld %p %d] %p %d/%d",
+                            current->tid, uaddr2, *uaddr2, w, result2, val2);
+                    }
+                    enqueue(f2->waiters, w);
+                }
             }
 
-            /* XXX -- what if w has a timeout registered on the old f? should it move
-             * to f2??
-             * we use the new timeout value, though from man futex it's not clear
-             * whether this should be used or not
-             */
-             (void)blockq_check_timeout(f2->bq, w,
-                closure(f2->h, futex_bh, f2, w, ts),
-                false, ts
-             );
+            return result + result2;
+        }
+        return -EAGAIN;
+
+    case FUTEX_WAKE_OP:
+        {
+            unsigned int cmparg = val3 & MASK(12);
+            unsigned int oparg = (val3 >> 12) & MASK(12);
+            unsigned int cmp = (val3 >> 24) & MASK(4);
+            unsigned int op = (val3 >> 28) & MASK(4);
+
+            if (verbose) {
+                thread_log(current, "futex_wake_op: [%ld %p %d] %p %d %d %d %d",
+                    current->tid, uaddr, *uaddr, uaddr2, cmparg, oparg, cmp, op);
+            }
+
+            int oldval = *(int *) uaddr2;
+            
+            switch (op) {
+            case FUTEX_OP_SET:   *uaddr2 = oparg; break;
+            case FUTEX_OP_ADD:   *uaddr2 += oparg; break;
+            case FUTEX_OP_OR:    *uaddr2 |= oparg; break;
+            case FUTEX_OP_ANDN:  *uaddr2 &= ~oparg; break;
+            case FUTEX_OP_XOR:   *uaddr2 ^= oparg; break;
+            }
+
+            int result = futex_wake(f, val, verbose, uaddr);
+            
+            int c = 0;
+            switch (cmp) {
+            case FUTEX_OP_CMP_EQ: c = (oldval == cmparg) ; break;
+            case FUTEX_OP_CMP_NE: c = (oldval != cmparg); break;
+            case FUTEX_OP_CMP_LT: c = (oldval < cmparg); break;
+            case FUTEX_OP_CMP_LE: c = (oldval <= cmparg); break;
+            case FUTEX_OP_CMP_GT: c = (oldval > cmparg) ; break;
+            case FUTEX_OP_CMP_GE: c = (oldval >= cmparg) ; break;
+            }
+            
+            if (c) {
+                fut f2 = soft_create_futex(current->p, u64_from_pointer(uaddr2));
+                result += futex_wake(f2, val2, verbose, uaddr2);
+            }
+
+            return result;
         }
 
-        return set_syscall_return(current, wake1 + wake2);
-    }
-
-    case FUTEX_WAKE_OP: {
-        unsigned int cmparg = val3 & MASK(12);
-        unsigned int oparg = (val3 >> 12) & MASK(12);
-        unsigned int cmp = (val3 >> 24) & MASK(4);
-        unsigned int op = (val3 >> 28) & MASK(4);
-        int oldval, wake1, wake2, c;
-
-        if (verbose) {
-            thread_log(current, "futex_wake_op: [%ld %p %d] %p %d %d %d %d",
-                current->tid, uaddr, *uaddr, uaddr2, cmparg, oparg, cmp, op);
+    case FUTEX_WAIT_BITSET:
+        if (*uaddr == val) {
+            if (verbose) {
+                thread_log(current, "futex_wait_bitset [%ld %p %d] %d %p %d",
+                    current->tid, uaddr, *uaddr, val, timeout, val3);
+            }
+            set_syscall_return(current, 0);
+            // TODO: timeout should be absolute based on CLOCK_REALTIME
+            if (timeout)
+                register_futex_timer(current, f, timeout);                          
+            enqueue(f->waiters, current);
+            thread_sleep_uninterruptible(); /* XXX move to blockq */
         }
+        return -EAGAIN;
 
-        oldval = *(int *) uaddr2;
-        
-        switch (op) {
-        case FUTEX_OP_SET:   *uaddr2 = oparg; break;
-        case FUTEX_OP_ADD:   *uaddr2 += oparg; break;
-        case FUTEX_OP_OR:    *uaddr2 |= oparg; break;
-        case FUTEX_OP_ANDN:  *uaddr2 &= ~oparg; break;
-        case FUTEX_OP_XOR:   *uaddr2 ^= oparg; break;
-        }
-
-        wake1 = futex_wake_many(f, val);
-        
-        c = 0;
-        switch (cmp) {
-        case FUTEX_OP_CMP_EQ: c = (oldval == cmparg) ; break;
-        case FUTEX_OP_CMP_NE: c = (oldval != cmparg); break;
-        case FUTEX_OP_CMP_LT: c = (oldval < cmparg); break;
-        case FUTEX_OP_CMP_LE: c = (oldval <= cmparg); break;
-        case FUTEX_OP_CMP_GT: c = (oldval > cmparg) ; break;
-        case FUTEX_OP_CMP_GE: c = (oldval >= cmparg) ; break;
-        }
-        
-        wake2 = 0;
-        if (c) {
-            struct futex * f2 = 
-                soft_create_futex(current->p, u64_from_pointer(uaddr2));
-            if (f2 == INVALID_ADDRESS)
-                return set_syscall_error(current, ENOMEM);
-
-            wake2 = futex_wake_many(f2, val2);
-        }
-
-        return set_syscall_return(current, wake1 + wake2);
-    }
-
-    case FUTEX_WAIT_BITSET: {
-        if (verbose)
-            thread_log(current, "futex_wait_bitset [%ld %p %d] %d 0x%ld %d",
-                current->tid, uaddr, *uaddr, val, val2, val3);
-
-        if (*uaddr != val)
-            return set_syscall_error(current, EAGAIN);
-
-        set_syscall_return(current, 0);
-        // TODO: timeout should be absolute based on CLOCK_REALTIME
-        return blockq_check_timeout(f->bq, current, 
-            closure(f->h, futex_bh, f, current, ts),
-            false, ts
-        );
-    }
-
-    case FUTEX_REQUEUE: rprintf("futex_requeue not implemented\n"); break;
     case FUTEX_WAKE_BITSET: rprintf("futex_wake_bitset not implemented\n"); break;
     case FUTEX_LOCK_PI: rprintf("futex_lock_pi not implemented\n"); break;
     case FUTEX_TRYLOCK_PI: rprintf("futex_trylock_pi not implemented\n"); break;
@@ -348,8 +264,7 @@ static sysreturn futex(int *uaddr, int futex_op, int val,
     case FUTEX_CMP_REQUEUE_PI: rprintf("futex_cmp_requeue_pi not implemented\n"); break;
     case FUTEX_WAIT_REQUEUE_PI: rprintf("futex_wait_requeue_pi not implemented\n"); break;
     }
-
-    return set_syscall_error(current, ENOSYS);
+    return 0;
 }
 
 void register_thread_syscalls(struct syscall *map)
@@ -450,20 +365,7 @@ thread create_thread(process p)
     // heap I guess
     static int tidcount = 0;
     heap h = heap_general((kernel_heaps)p->uh);
-
     thread t = allocate(h, sizeof(struct thread));
-    if (t == INVALID_ADDRESS) {
-        msg_err("failed to allocate thread\n");
-        return INVALID_ADDRESS;
-    }
-
-    t->dummy_blockq = allocate_blockq(h, "dummy");
-    if (t->dummy_blockq == INVALID_ADDRESS) {
-        msg_err("failed to allocate blockq\n");
-        deallocate(h, t, sizeof(struct thread));
-        return INVALID_ADDRESS;
-    }
-
     t->p = p;
     t->syscall = -1;
     t->uh = *p->uh;
@@ -476,6 +378,7 @@ thread create_thread(process p)
     t->run = closure(h, run_thread, t);
     vector_push(p->threads, t);
     t->blocked_on = 0;
+    t->dummy_blockq = allocate_blockq(h, "dummy", 1, 0);
     init_sigstate(&t->signals);
     t->dispatch_sigstate = 0;
 
