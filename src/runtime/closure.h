966b0f44860996adbac605c2c533bea01140d4bd
William Jhun <wjhun@oswego.edu>
2019-09-27 18:36:29 -0400

    add closure_self() to allow closure reuse, add deallocate_closure() and closure_finish(); unit test dealloc and self-apply

--- src/runtime/closure.h
+++ src/runtime/closure.h
@@ -2,7 +2,7 @@
 
 #define closure_type(__x, __ret, ...) __ret (**__x)(void *, ## __VA_ARGS__)
 
-#define apply(__c, ...) (*__c)(__c, ## __VA_ARGS__)
+#define apply(__c, ...) (*(__c))((void *)(__c), ## __VA_ARGS__)
 
 #define __closure(__h, __p, __s, __name, ...)    \
     _fill_##__name(__h, __p, __s, ##__VA_ARGS__)
@@ -23,8 +23,20 @@ struct _closure_common {
     bytes size;
 };
 
-#define bound(name) (__self->name)
 #define __closure_define(nl, nr) CLOSURE_ ## nl ## _ ## nr
 #define closure_function(nl, nr, ...) __closure_define(nl, nr)(__VA_ARGS__)
+
+#define bound(v) (__self->v)
+#define closure_self() (&(__self->__apply))
+
+/* XXX type safety, possibly tag */
+static inline void deallocate_closure(void *p)
+{
+    struct _closure_common *c = p + sizeof(void *); /* skip __apply */
+    if (c->h && c->size > 0)
+        deallocate(c->h, p, c->size);
+}
+
+#define closure_finish() do { deallocate_closure(__self); __self = 0; } while(0)
 
 #include <closure_templates.h>
