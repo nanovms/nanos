ac4de87093e14cea3a8063de97ac22b76d45416f
William Jhun <36206676+wjhun@users.noreply.github.com>
2019-12-13 10:28:24 -0500

    rebase conflicts

--- src/unix/syscall.c
+++ src/unix/syscall.c
@@ -200,7 +200,6 @@ void register_other_syscalls(struct syscall *map)
     register_syscall(map, move_pages, 0);
     register_syscall(map, utimensat, 0);
     register_syscall(map, fallocate, 0);
-    register_syscall(map, signalfd4, 0);
     register_syscall(map, inotify_init1, 0);
     register_syscall(map, preadv, 0);
     register_syscall(map, pwritev, 0);
