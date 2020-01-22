8b5e7f87650fb20e68dc2468985db9251c6249f6
William Jhun <36206676+wjhun@users.noreply.github.com>
2020-01-22 10:49:54 -0500

    fix up spinlock asm

--- src/x86_64/lock.h
+++ src/x86_64/lock.h
@@ -1,16 +1,16 @@
 static inline boolean spin_try(u64 *loc) {
-    u64 tmp;
-    // no pause!?
-    __asm__ ("lock xchg %0, %1":"=m"(*loc),"=r"(tmp));
+    u64 tmp = 1;
+    /* Is it the compare and branch to skip a pause? */
+    asm volatile("lock xchg %0, %1; cmp $1, %1; jne 1f; pause; 1:" : "+m"(*loc), "+r"(tmp) :: "memory");
     return tmp == 0 ? true:false;
 }
 
 static inline void spin_lock(u64 *loc) {
     u64 tmp = 1;
-    // no pause!?
-    __asm__ ("__spin:lock xchg %0, %1; test %1, 1; jne __spin":"=m"(*loc),"=r"(tmp));
+    asm volatile("1: lock xchg %0, %1; cmp $1, %1; jne 2f; pause; jmp 1b; 2:" : "+m"(*loc), "+r"(tmp) :: "memory");
 }
 
 static inline void spin_unlock(u64 *loc) {
-    *loc = 0;
+    compiler_barrier();
+    *(volatile u64 *)loc = 0;
 }
