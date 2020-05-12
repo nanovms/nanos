b4cb3596146faff45a38879a91bf188414d30e88
Francesco Lavra <francescolavra.fl@gmail.com>
2020-05-12 08:40:14 +0200

    Syscalls: add getrusage
    The current implementation retrieves the total amount of time spent
    executing, both in user mode and in system mode.
    
    Closes #1161.

--- src/unix/system_structs.h
+++ src/unix/system_structs.h
@@ -303,6 +303,29 @@ struct rlimit {
 #define RLIMIT_RTPRIO		14	/* maximum realtime priority */
 #define RLIMIT_RTTIME		15	/* timeout for RT tasks in us */
 
+#define RUSAGE_SELF     0
+#define RUSAGE_CHILDREN (-1)
+#define RUSAGE_THREAD   1
+
+struct rusage {
+    struct timeval ru_utime;
+    struct timeval ru_stime;
+    long ru_maxrss;
+    long ru_ixrss;
+    long ru_idrss;
+    long ru_isrss;
+    long ru_minflt;
+    long ru_majflt;
+    long ru_nswap;
+    long ru_inblock;
+    long ru_oublock;
+    long ru_msgsnd;
+    long ru_msgrcv;
+    long ru_nsignals;
+    long ru_nvcsw;
+    long ru_nivcsw;
+};
+
 #define SIGHUP		 1
 #define SIGINT		 2
 #define SIGQUIT		 3
