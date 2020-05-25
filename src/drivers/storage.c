001ec758afff61a8fd2fc25d85ebd30db934a4cb
Max Khon <max@nanovms.com>
2020-05-25 13:06:06 +0700

    Initial implementation of vmware pvscsi driver

--- src/drivers/storage.c
+++ src/drivers/storage.c
@@ -1,10 +1,12 @@
 #include <kernel.h>
 #include <virtio/virtio.h>
+#include <vmware/storage.h>
 #include <drivers/ata-pci.h>
 
 void init_storage(kernel_heaps kh, storage_attach a)
 {
     virtio_register_blk(kh, a);
     virtio_register_scsi(kh, a);
+    pvscsi_register(kh, a);
     ata_pci_register(kh, a);
 }
