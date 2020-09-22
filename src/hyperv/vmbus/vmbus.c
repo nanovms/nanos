e8ddbba0c49e8e42cb63428afa5c5a554ff2ecaf
William Jhun <36206676+wjhun@users.noreply.github.com>
2020-09-22 13:05:37 -0400

    vmbus: parameterize scheduling queue on channel open

--- src/hyperv/vmbus/vmbus.c
+++ src/hyperv/vmbus/vmbus.c
@@ -96,7 +96,7 @@ vmbus_handle_intr1(vmbus_dev sc, int cpu)
     msg = msg_base + VMBUS_SINT_MESSAGE;
     if (msg->msg_type != HYPERV_MSGTYPE_NONE) {
         vmbus_debug("SINT Message!");
-        enqueue(bhqueue, VMBUS_PCPU_GET(sc, message_task, cpu));
+        enqueue(runqueue, VMBUS_PCPU_GET(sc, message_task, cpu));
     }
 }
 
