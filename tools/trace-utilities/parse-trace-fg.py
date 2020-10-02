6722f98b166aec7bd84e784ffb211ef8d8f82850
Justin Sanders <sanderssj@gmail.com>
2020-10-02 11:14:37 -0400

    Added new ftrace analysis tools

--- tools/trace-utilities/parse-trace-fg.py
+++ tools/trace-utilities/parse-trace-fg.py
@@ -8,22 +8,21 @@ import csv
 # dictionary of thread stacks
 thread_stacks = {}
 
-# function latencies
-function_times = []
-
-def update_parent(child_time, tid):
-    if len(thread_stacks[tid]) == 0:
-        return
-
-    thread_stacks[tid][-1]["child_duration"] += child_time
+# stack time track
+stack_times = {}
 
 def parse_entry(match, tid):
     function = match.group(1)
     thread_stacks[tid].append({
         "function" : function,
-        "child_duration" : 0
     })
     return tid
+
+def record_stack_time(stk, tm):
+    if stk in stack_times:
+        stack_times[stk] += tm
+    else:
+        stack_times[stk] = tm
 
 def parse_return(match, tid):
     time = float(match.group(1))
@@ -36,17 +35,13 @@ def parse_return(match, tid):
         return tid
 
     function = dat["function"]
-    child_time = dat["child_duration"]
+    stk = ""
+    for f in thread_stacks[tid]:
+        stk += f["function"]
+        if f["function"] != function:
+            stk += ";"
     del thread_stacks[tid][-1]
-
-    update_parent(time, tid)
-
-    function_times.append({
-        "tid" : tid,
-        "latency_us" : time,
-        "latency_us_self" : time - child_time,
-        "function" : function
-    })
+    record_stack_time(stk, time)
 
     return tid
 
@@ -54,25 +49,13 @@ def parse_leaf(match, tid):
     time = float(match.group(1))
     function = match.group(2)
 
-    update_parent(time, tid)
-
-    function_times.append({
-        "tid" : tid,
-        "latency_us" : time,
-        "latency_us_self" : time,
-        "function" : function
-    })
+    stk = ""
+    for f in thread_stacks[tid]:
+        stk += f["function"] + ";"
+    stk += function
+    record_stack_time(stk, time)
 
     return tid
-
-def parse_switch(match, tid):
-    tid_out = int(match.group(1))
-    tid_in = int(match.group(2))
-    assert tid_out == tid
-
-    if tid_in not in thread_stacks:
-        thread_stacks[tid_in] = []
-    return tid_in
 
 def parse_null(match, tid):
     return tid
@@ -103,15 +86,11 @@ def parse_trace(trace):
     pat_entry = re.compile(preamble + no_time + bar + entry + to_end)
     pat_leaf = re.compile(preamble + time + bar + leaf + to_end)
     pat_return = re.compile(preamble + time + bar + ret + to_end)
-    pat_switch = re.compile(r'------------------------------------------')
-    pat_thread_switch = re.compile(r' 0\) [a-zA-Z0-9_]*-([0-9]+)  => [a-zA-Z0-9_]*-([0-9]+)[ ]*$')
 
     parsers = [
         Parser(pat_entry, parse_entry),
         Parser(pat_leaf, parse_leaf),
         Parser(pat_return, parse_return),
-        Parser(pat_switch, parse_null),
-        Parser(pat_thread_switch, parse_switch)
     ]
 
     current = 0
@@ -127,13 +106,9 @@ def parse_trace(trace):
             else:
                 print "Unmatched line: %s" % line
 
-    global function_times
-    csv_columns = ['function', 'tid', 'latency_us', 'latency_us_self']
-    with open('trace.csv', 'w') as f:
-        writer = csv.DictWriter(f, fieldnames=csv_columns)
-        writer.writeheader()
-        for d in function_times:
-            writer.writerow(d)
+    with open('tracefg.out', 'w') as f:
+        for s in sorted(stack_times.iterkeys()):
+            f.write('{0} {1:0f}\n'.format(s, stack_times[s]))
 
 def main(argv):
     argc = len(argv)
