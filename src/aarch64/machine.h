nanovms/nanos

commit 29c970f955e85385882d07b1cfb1afdf1605c9f8

CPU timers: add support for SMP
This commit adds thread-specific timers that measure the CPU time spent (in
either user mode or system mode) by each thread. Measurement of CPU time spent
by the entire process is now implemented by taking into account that different
threads could run simultaneously on different CPUs.

Closes #1162.

src/unix/thread.c
@@ -153,7 +153,7 @@ static inline void run_thread_frame(thread t)
     thread old = current;
     current_cpu()->current_thread = t;
     ftrace_thread_switch(old, current);    /* ftrace needs to know about the switch event */
-    proc_enter_user(current->p);
+    thread_enter_user(old, t);
 
     /* cover wake-before-sleep situations (e.g. sched yield, fs ops that don't go to disk, etc.) */
     t->blocked_on = 0;
@@ -257,6 +257,7 @@ define_closure_function(1, 0, void, free_thread,
 define_closure_function(1, 0, void, resume_syscall, thread, t)
 {
     current_cpu()->current_thread = bound(t);
+    thread_resume(bound(t));
     syscall_debug(thread_frame(bound(t)));
 }
 
@@ -311,6 +312,9 @@ thread create_thread(process p)
         deallocate(h, t, sizeof(struct thread));
         return INVALID_ADDRESS;
     }
+    t->sysctx = false;
+    t->utime = t->stime = 0;
+    t->start_time = now(CLOCK_ID_MONOTONIC);
 
     // XXX sigframe
     vector_set(p->threads, t->tid, t);

src/unix/unix.c
@@ -279,9 +279,7 @@ process create_process(unix_heaps uh, tuple root, filesystem fs)
     create_stdfiles(uh, p);
     init_threads(p);
     p->syscalls = linux_syscalls;
-    p->sysctx = false;
     p->utime = p->stime = 0;
-    p->start_time = now(CLOCK_ID_MONOTONIC);
     init_sigstate(&p->signals);
     zero(p->sigactions, sizeof(p->sigactions));
     p->posix_timer_ids = create_id_heap(h, h, 0, U32_MAX, 1);
@@ -292,58 +290,89 @@ process create_process(unix_heaps uh, tuple root, filesystem fs)
     return p;
 }
 
-void proc_enter_user(process p)
+void thread_enter_user(thread out, thread in)
 {
-    if (p->sysctx) {
+    if (out)
+        thread_pause(out);
+    thread_resume(in);
+    in->sysctx = false;
+}
+
+void thread_enter_system(thread t)
+{
+    if (!t->sysctx) {
         timestamp here = now(CLOCK_ID_MONOTONIC);
-        p->stime += here - p->start_time;
-        p->sysctx = false;
-        p->start_time = here;
+        timestamp diff = here - t->start_time;
+        fetch_and_add_64(&t->p->utime, diff);
+        t->utime += diff;
+        t->start_time = here;
+        t->sysctx = true;
     }
 }
 
-void proc_enter_system(process p)
+void thread_pause(thread t)
 {
-    if (!p->sysctx) {
-        timestamp here = now(CLOCK_ID_MONOTONIC);
-        p->utime += here - p->start_time;
-        p->sysctx = true;
-        p->start_time = here;
-    }
-}
-
-void proc_pause(process p)
-{
-    timestamp here = now(CLOCK_ID_MONOTONIC);
-    if (p->sysctx) {
-        p->stime += here - p->start_time;
+    process p = t->p;
+    timestamp diff = now(CLOCK_ID_MONOTONIC) - t->start_time;
+    if (t->sysctx) {
+        fetch_and_add_64(&p->stime, diff);
+        t->stime += diff;
     }
     else {
-        p->utime += here - p->start_time;
+        fetch_and_add_64(&p->utime, diff);
+        t->utime += diff;
     }
 }
 
-void proc_resume(process p)
+void thread_resume(thread t)
 {
-    p->start_time = now(CLOCK_ID_MONOTONIC);
+    t->start_time = now(CLOCK_ID_MONOTONIC);
+}
+
+static timestamp utime_updated(thread t)
+{
+    timestamp ts = t->utime;
+    if (!t->sysctx)
+        ts += now(CLOCK_ID_MONOTONIC) - t->start_time;
+    return ts;
+}
+
+static timestamp stime_updated(thread t)
+{
+    timestamp ts = t->stime;
+    if (t->sysctx)
+        ts += now(CLOCK_ID_MONOTONIC) - t->start_time;
+    return ts;
 }
 
 timestamp proc_utime(process p)
 {
     timestamp utime = p->utime;
-    if (!p->sysctx) {
-        utime += now(CLOCK_ID_MONOTONIC) - p->start_time;
-    }
+    thread t;
+    vector_foreach(p->threads, t)
+        if (t)
+            utime += utime_updated(t);
     return utime;
 }
 
 timestamp proc_stime(process p)
 {
     timestamp stime = p->stime;
-    if (p->sysctx) {
-        stime += now(CLOCK_ID_MONOTONIC) - p->start_time;
-    }
+    thread t;
+    vector_foreach(p->threads, t)
+        if (t)
+            stime += stime_updated(t);
     return stime;
+}
+
+timestamp thread_utime(thread t)
+{
+    return utime_updated(t);
+}
+
+timestamp thread_stime(thread t)
+{
+    return stime_updated(t);
 }
 
 process init_unix(kernel_heaps kh, tuple root, filesystem fs)

src/unix/unix_internal.h
@@ -234,6 +234,10 @@ typedef struct thread {
     /* for waiting on thread-specific conditions rather than a resource */
     blockq thread_bq;
 
+    boolean sysctx;
+    timestamp utime, stime;
+    timestamp start_time;
+
     /* signals pending and saved state */
     struct sigstate signals;
     sigstate dispatch_sigstate; /* while signal handler in flight, save sigstate */
@@ -337,9 +341,7 @@ typedef struct process {
     rangemap          vmaps;    /* process mappings */
     vmap              stack_map;
     vmap              heap_map;
-    boolean           sysctx;
     timestamp         utime, stime;
-    timestamp         start_time;
     struct sigstate   signals;
     struct sigaction  sigactions[NSIG];
     id_heap           posix_timer_ids;

src/unix/syscall.c
@@ -2215,7 +2215,7 @@ void syscall_debug(context f)
     }
     sysreturn (*h)(u64, u64, u64, u64, u64, u64) = s->handler;
     if (h) {
-        proc_enter_system(current->p);
+        thread_enter_system(current);
 
         sysreturn rv = h(f[FRAME_RDI], f[FRAME_RSI], f[FRAME_RDX], f[FRAME_R10], f[FRAME_R8], f[FRAME_R9]);
         set_syscall_return(current, rv);
@@ -2261,6 +2261,7 @@ static void syscall_schedule(context f, u64 call)
         current_cpu()->state = cpu_kernel;
         syscall_debug(f);
     } else {
+        thread_pause(current);
         enqueue(runqueue, &current->deferred_syscall);
         runloop();
     }

src/unix/unix.h
@@ -8,10 +8,13 @@ process create_process(unix_heaps uh, tuple root, filesystem fs);
 thread create_thread(process p);
 process exec_elf(buffer ex, process kernel_process);
 
-void proc_enter_user(process p);
-void proc_enter_system(process p);
-void proc_pause(process p);
-void proc_resume(process p);
+void thread_enter_user(thread out, thread in);
+void thread_enter_system(thread t);
+void thread_pause(thread t);
+void thread_resume(thread t);
 
 timestamp proc_utime(process p);
 timestamp proc_stime(process p);
+
+timestamp thread_utime(thread t);
+timestamp thread_stime(thread t);

src/x86_64/schedule.c
@@ -160,6 +160,8 @@ NOTRACE void __attribute__((noreturn)) runloop_internal()
 
     if ((t = dequeue(thread_queue)) != INVALID_ADDRESS)
         run_thunk(t, cpu_user);
+    if (ci->current_thread)
+        thread_pause(ci->current_thread);
 
     kernel_sleep();
 }    

src/x86_64/x86_64.h
@@ -104,6 +104,11 @@ static inline void atomic_clear_bit(u64 *target, u64 bit)
     asm volatile("lock btrq %1, %0": "+m"(*target):"r"(bit) : "memory");
 }
 
+static inline u64 fetch_and_add_64(u64 *target, u64 num)
+{
+    return __sync_fetch_and_add(target, num);
+}
+
 #include <lock.h>
 
 extern u64 read_msr(u64);

commit 7222ff86ac0f02abc75dc84e3d65f6366f92f09b

clock_gettime(): fix thread and process time accounting
Commit 29c970f955e85385882d07b1cfb1afdf1605c9f8 introduced a
regression which caused CPU time for the threads of a process to be
accounted twice in the CPU time of the process. This commit removes
the utime and stime members from struct process, which are not
necessary because the times of the individual threads need to be
summed anyway when the process CPU time is requested from
userspace.
In addition, calling set_current_thread() directly from
run_thread_frame() messes up time accounting, because when
thread_enter_user() is called it sees that the provided thread is
already the current thread. This is now fixed.

fetch_and_add_64() is now unused and is being removed; the existing
fetch_and_add() for x86 is being changed to use the same
implementation as fetch_and_add_64(), because, as noted in
https://github.com/nanovms/nanos/pull/1171#discussion_r423757729,
the compiler-generated code is a little better.

src/aarch64/machine.h
@@ -160,11 +160,6 @@ static inline word fetch_and_add(word *target, word num)
     return __sync_fetch_and_add(target, num);
 }
 
-static inline u64 fetch_and_add_64(u64 *target, u64 num)
-{
-    return __sync_fetch_and_add(target, num);
-}
-
 static inline void kern_pause(void)
 {
   // XXX

src/unix/thread.c
@@ -151,9 +151,8 @@ static inline void run_thread_frame(thread t)
     check_stop_conditions(t);
     kern_lock(); // xx - make thread entry a separate exclusion region for performance
     thread old = current;
-    set_current_thread((nanos_thread)t);
-    ftrace_thread_switch(old, current);    /* ftrace needs to know about the switch event */
     thread_enter_user(t);
+    ftrace_thread_switch(old, t);    /* ftrace needs to know about the switch event */
 
     /* cover wake-before-sleep situations (e.g. sched yield, fs ops that don't go to disk, etc.) */
     t->blocked_on = 0;

src/unix/unix.c
@@ -331,7 +331,6 @@ process create_process(unix_heaps uh, tuple root, filesystem fs)
     create_stdfiles(uh, p);
     init_threads(p);
     p->syscalls = linux_syscalls;
-    p->utime = p->stime = 0;
     init_sigstate(&p->signals);
     zero(p->sigactions, sizeof(p->sigactions));
     p->posix_timer_ids = create_id_heap(h, h, 0, U32_MAX, 1);
@@ -353,7 +352,6 @@ void thread_enter_system(thread t)
     if (!t->sysctx) {
         timestamp here = now(CLOCK_ID_MONOTONIC);
         timestamp diff = here - t->start_time;
-        fetch_and_add_64(&t->p->utime, diff);
         t->utime += diff;
         t->start_time = here;
         t->sysctx = true;
@@ -365,14 +363,11 @@ void thread_pause(thread t)
 {
     if (get_current_thread() != &t->thrd)
         return;
-    process p = t->p;
     timestamp diff = now(CLOCK_ID_MONOTONIC) - t->start_time;
     if (t->sysctx) {
-        fetch_and_add_64(&p->stime, diff);
         t->stime += diff;
     }
     else {
-        fetch_and_add_64(&p->utime, diff);
         t->utime += diff;
     }
     set_current_thread(0);
@@ -404,7 +399,7 @@ static timestamp stime_updated(thread t)
 
 timestamp proc_utime(process p)
 {
-    timestamp utime = p->utime;
+    timestamp utime = 0;
     thread t;
     vector_foreach(p->threads, t)
         if (t)
@@ -414,7 +409,7 @@ timestamp proc_utime(process p)
 
 timestamp proc_stime(process p)
 {
-    timestamp stime = p->stime;
+    timestamp stime = 0;
     thread t;
     vector_foreach(p->threads, t)
         if (t)

src/unix/unix_internal.h
@@ -413,7 +413,6 @@ typedef struct process {
     rangemap          vmaps;    /* process mappings */
     vmap              stack_map;
     vmap              heap_map;
-    timestamp         utime, stime;
     struct sigstate   signals;
     struct sigaction  sigactions[NSIG];
     id_heap           posix_timer_ids;

src/x86_64/machine.h
@@ -50,8 +50,7 @@ static inline void memory_barrier(void)
 
 static inline word fetch_and_add(word *variable, word value)
 {
-    asm volatile("lock; xadd %0, %1" : "+r" (value), "+m" (*variable) :: "memory", "cc");
-    return value;
+    return __sync_fetch_and_add(variable, value);
 }
 
 static inline void atomic_set_bit(u64 *target, u64 bit)
@@ -62,11 +61,6 @@ static inline void atomic_set_bit(u64 *target, u64 bit)
 static inline void atomic_clear_bit(u64 *target, u64 bit)
 {
     asm volatile("lock btrq %1, %0": "+m"(*target):"r"(bit) : "memory");
-}
-
-static inline u64 fetch_and_add_64(u64 *target, u64 num)
-{
-    return __sync_fetch_and_add(target, num);
 }
 
 static inline void kern_pause(void)