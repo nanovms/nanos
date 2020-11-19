5e0ff6c3ed95673cec2bd19013c0385a2edac8c6
Uma Sethuraman <uma.sethuraman@utexas.edu>
2020-11-19 12:17:40 -0600

    added test for futex_wake

--- test/runtime/futex_test.c
+++ test/runtime/futex_test.c
@@ -1,18 +1,47 @@
-#include <stdlib.h>
-#include <runtime.h>
-#include <stdbool.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <runtime.h>
+
 #define EXIT_FAILURE 1
 #define EXIT_SUCCESS 0
+#define FUTEX_WAIT 0
+#define FUTEX_WAKE 1
+
+static void * basic_test_thread(void *arg) {
+    return (void *)EXIT_SUCCESS;
+}
+
+static int futex_wake_test() {
+    pthread_t pt;
+    int ret;
+    if (pthread_create(&pt, NULL, basic_test_thread, NULL)) {
+        printf("unable to create thread\n");
+        ret = -1;
+    }
+    else {
+        ret = syscall(SYS_futex, (int*)(&pt), FUTEX_WAKE, pt, 0, (int*)(&pt), 0);
+    }
+    return ret;
+}
+
+boolean basic_test() {
+    if (futex_wake_test() != 0) {
+        return false;
+    }
+
+    return true;
+}
 
 int main (int argc, char* argv[]) {
-    bool passed = true;
-    if (!passed) {
-        printf("tests failed");
-        msg_err("test failed");
+    if (basic_test()) {
+        printf("futex tests passed\n");
+        exit(EXIT_SUCCESS);
+    }
+    else {
+        printf("futex tests failed\n");
         exit(EXIT_FAILURE);
     }
-    
-    printf("tests passed\n");
-    exit(EXIT_SUCCESS);      
 }
