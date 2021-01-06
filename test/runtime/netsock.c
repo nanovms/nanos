9c695ae738e1a9e8aef53a0e25fc9ce0b6d9d68d
Francesco Lavra <francescolavra.fl@gmail.com>
2021-01-06 21:22:48 +0100

    Network sockets: fix handling of closed connections
    When a thread is blocked on a socket read, write, or accept, and
    the remote peer closes the connection (or aborts a connection
    attempt), wakeup_sock(s, WAKEUP_SOCK_EXCEPT) is called on the
    socket, which invokes the bottom halves with a
    BLOCKQ_ACTION_NULLIFY flag; this causes the syscalls to return
    -ERESTARTSYS, which is wrong.
    This commit fixes the above issue by moving the check for
    BLOCKQ_ACTION_NULLIFY after the checks for error conditions.
    In addition, syscalls such as read(), recvfrom() and recvmsg()
    should return 0 (instead of -ENOTCONN) when invoked on a socket
    that was previously connected but whose connection is closed
    (either by the remote peer, or by the local node).
    A new test case is being added to the netsock runtime test to
    verify the correct behavior in the above scenarios.

--- test/runtime/netsock.c
+++ test/runtime/netsock.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -80,11 +81,106 @@ static void netsock_test_fionread(void)
     test_assert(close(fd) == 0);
 }
 
+/* Connect to TCP server and then close connection right away. */
+static void *netsock_test_connclose_thread(void *arg)
+{
+    int port = (long)arg;
+    int fd;
+    struct sockaddr_in addr;
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    test_assert(fd > 0);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    test_assert(connect(fd, (struct sockaddr *)&addr, sizeof(addr)) == 0);
+    test_assert(close(fd) == 0);
+    return NULL;
+}
+
+/* Connect to TCP server and wait for connection to be closed. */
+static void *netsock_test_connwait_thread(void *arg)
+{
+    int port = (long)arg;
+    int fd;
+    struct sockaddr_in addr;
+    uint8_t buf[8];
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    test_assert(fd > 0);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    test_assert(connect(fd, (struct sockaddr *)&addr, sizeof(addr)) == 0);
+    test_assert(read(fd, buf, sizeof(buf)) == 0);
+    test_assert(close(fd) == 0);
+    return NULL;
+}
+
+/* Tests behavior of syscalls invoked on a socket after the connection has been closed. */
+static void netsock_test_connclosed(void)
+{
+    int fd, conn_fd;
+    struct sockaddr_in addr;
+    const int port = 1234;
+    pthread_t pt;
+    uint8_t buf[8];
+    struct iovec iov;
+    struct msghdr msg;
+    int ret;
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    test_assert(fd > 0);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    test_assert(bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == 0);
+    test_assert(listen(fd, 1) == 0);
+
+    /* Test connection closed by peer. */
+    ret = pthread_create(&pt, NULL, netsock_test_connclose_thread,
+        (void *)(long)port);
+    test_assert(ret == 0);
+    conn_fd = accept(fd, NULL, NULL);
+    test_assert(conn_fd > 0);
+    test_assert(read(conn_fd, buf, sizeof(buf)) == 0);
+    test_assert(recv(conn_fd, buf, sizeof(buf), 0) == 0);
+    test_assert(recvfrom(conn_fd, buf, sizeof(buf), 0, NULL, 0) == 0);
+    memset(&iov, 0, sizeof(iov));
+    memset(&msg, 0, sizeof(msg));
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+    test_assert(recvmsg(conn_fd, &msg, 0) == 0);
+    test_assert(pthread_join(pt, NULL) == 0);
+    test_assert(close(conn_fd) == 0);
+
+    /* Test connection closed via shutdown(). */
+    ret = pthread_create(&pt, NULL, netsock_test_connwait_thread,
+        (void *)(long)port);
+    test_assert(ret == 0);
+    conn_fd = accept(fd, NULL, NULL);
+    test_assert(conn_fd > 0);
+    test_assert(shutdown(conn_fd, SHUT_RDWR) == 0);
+    test_assert(read(conn_fd, buf, sizeof(buf)) == 0);
+    test_assert(recv(conn_fd, buf, sizeof(buf), 0) == 0);
+    test_assert(recvfrom(conn_fd, buf, sizeof(buf), 0, NULL, 0) == 0);
+    memset(&iov, 0, sizeof(iov));
+    memset(&msg, 0, sizeof(msg));
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+    test_assert(recvmsg(conn_fd, &msg, 0) == 0);
+    test_assert(pthread_join(pt, NULL) == 0);
+    test_assert(close(conn_fd) == 0);
+
+    test_assert(close(fd) == 0);
+}
+
 int main(int argc, char **argv)
 {
     setbuf(stdout, NULL);
 
     netsock_test_fionread();
+    netsock_test_connclosed();
     printf("Network socket tests OK\n");
     return EXIT_SUCCESS;
 }
