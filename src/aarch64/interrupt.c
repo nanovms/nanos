a6f6c58a9c7b9d1398f5ce1a7b8414e5cb43463d
William Jhun <36206676+wjhun@users.noreply.github.com>
2021-01-18 18:12:36 -0500

    support shared interrupts / multiple handlers per irq

--- src/aarch64/interrupt.c
+++ src/aarch64/interrupt.c
@@ -13,11 +13,14 @@
 #define INTERRUPT_VECTOR_START 32 /* end of exceptions; defined by architecture */
 #define MAX_INTERRUPT_VECTORS  256 /* as defined by architecture; we may have less */
 
-static const char *interrupt_names[MAX_INTERRUPT_VECTORS] = {
-    "dummy",
-};
+typedef struct inthandler {
+    struct list l;
+    thunk t;
+    const char *name;
+} *inthandler;
 
-static thunk *handlers;
+static struct list *handlers;
+
 extern u32 n_interrupt_vectors;
 extern u32 interrupt_vector_size;
 extern void * interrupt_vectors;
@@ -52,9 +55,12 @@ void print_frame(context f)
     console(" interrupt: ");
     print_u64(v);
     if (v < INTERRUPT_VECTOR_START) {
-        console(" (");
-        console((char *)interrupt_names[v]);
-        console(")");
+        list_foreach(&handlers[v], l) {
+            inthandler h = struct_from_list(l, inthandler, l);
+            console(" (");
+            console((char *)h->name);
+            console(")");
+        }
     }
     console("\n     frame: ");
     print_u64_with_sym(u64_from_pointer(f));
@@ -118,8 +124,10 @@ void print_stack(context c)
 {
     console("\nstack trace:\n");
     u64 *x = pointer_from_u64(c[FRAME_SP]);
-//    u64 *top = pointer_from_u64(c[FRAME_STACK_TOP]);
-    for (u64 i = 0; i < STACK_TRACE_DEPTH && ((void*)x) < pointer_from_u64(0xffff000000020000ull); i++) {
+
+    /* XXX fixme */
+    for (u64 i = 0; i < STACK_TRACE_DEPTH && ((void*)x) <
+             pointer_from_u64(0xffff000000020000ull); i++) {
         print_u64(u64_from_pointer(x));
         console(":   ");
         print_u64_with_sym(*x++);
@@ -135,16 +143,9 @@ void synchronous_handler(void)
 {
     cpuinfo ci = current_cpu();
     context f = ci->running_frame;
-
-#if 0
-    early_debug("\ncaught exception, el ");
-    early_debug_u64(f[FRAME_EL]);
-    early_debug("\n\n");
-    print_frame(f);
-    print_stack(f);
-#endif
-
     u32 esr = esr_from_frame(f);
+
+    int_debug("caught exception, EL%d, esr 0x%x\n", f[FRAME_EL], esr);
 
     if (field_from_u64(esr, ESR_EC) == ESR_EC_SVC_AARCH64 && (esr & ESR_IL) &&
         field_from_u64(esr, ESR_ISS_IMM16) == 0) {
@@ -181,17 +182,22 @@ void irq_handler(void)
     int_debug("%s: enter\n", __func__);
 
     while ((i = gic_dispatch_int()) != INTID_NO_PENDING) {
-        int_debug("[%2d] # %d (%s), state %s, el %d, frame %p, elr 0x%lx, spsr_esr 0x%lx\n",
-                  ci->id, i, interrupt_names[i], state_strings[ci->state], f[FRAME_EL],
+        int_debug("[%2d] # %d, state %s, EL%d, frame %p, elr 0x%lx, spsr_esr 0x%lx\n",
+                  ci->id, i, state_strings[ci->state], f[FRAME_EL],
                   f, f[FRAME_ELR], f[FRAME_ESR_SPSR]);
 
-        if (handlers[i]) {
-            int_debug("   invoking handler %F\n", handlers[i]);
+        if (list_empty(&handlers[i]))
+            halt("no handler for interrupt %d\n", i);
+
+        list_foreach(&handlers[i], l) {
+            inthandler h = struct_from_list(l, inthandler, l);
+            int_debug("   invoking handler %s (%F)\n", h->name, h->t);
             ci->state = cpu_interrupt;
-            apply(handlers[i]);
-            int_debug("   eoi %d\n", i);
-            gic_eoi(i);
+            apply(h->t);
         }
+
+        int_debug("   eoi %d\n", i);
+        gic_eoi(i);
     }
 
     if (is_current_kernel_context(f))
@@ -213,7 +219,9 @@ void invalid_handler(void)
 }
 
 static id_heap interrupt_vector_heap;
+static heap int_general;
 
+/* XXX TODO: no-op on non-MSI platform? */
 u64 allocate_interrupt(void)
 {
     return allocate_u64((heap)interrupt_vector_heap, 1);
@@ -231,26 +239,35 @@ boolean reserve_interrupt(u64 irq)
 
 void register_interrupt(int vector, thunk t, const char *name)
 {
-    int_debug("%s: vector %d, thunk %p (%F), name %s\n",
-              __func__, vector, t, t, name);
-    if (handlers[vector])
-        halt("%s: handler for vector %d already registered (%p)\n",
-             __func__, vector, handlers[vector]);
-    handlers[vector] = t;
-    interrupt_names[vector] = name;
+    boolean initialized = !list_empty(&handlers[vector]);
+    int_debug("%s: vector %d, thunk %p (%F), name %s%s\n",
+              __func__, vector, t, t, name, initialized ? ", shared" : "");
 
-    gic_set_int_priority(vector, 0);
-    gic_clear_pending_int(vector);
-    gic_enable_int(vector);
+    inthandler h = allocate(int_general, sizeof(struct inthandler));
+    assert(h != INVALID_ADDRESS);
+    h->t = t;
+    h->name = name;
+    list_insert_before(&handlers[vector], &h->l);
+
+    if (!initialized) {
+        gic_set_int_priority(vector, 0);
+        gic_clear_pending_int(vector);
+        gic_enable_int(vector);
+    }
 }
 
 void unregister_interrupt(int vector)
 {
+    int_debug("%s: vector %d\n", __func__, vector);
     gic_disable_int(vector);
-    if (!handlers[vector])
+    if (list_empty(&handlers[vector]))
         halt("%s: no handler registered for vector %d\n", __func__, vector);
-    handlers[vector] = 0;
-    interrupt_names[vector] = 0;
+    list_foreach(&handlers[vector], l) {
+        inthandler h = struct_from_list(l, inthandler, l);
+        int_debug("   remove handler %s (%F)\n", h->name, h->t);
+        list_delete(&h->l);
+        deallocate(int_general, h, sizeof(struct inthandler));
+    }
 }
 
 extern void *exception_vectors;
@@ -265,10 +282,13 @@ closure_struct(arm_timer, _timer);
 
 void init_interrupts(kernel_heaps kh)
 {
-    heap general = heap_general(kh);
-    handlers = allocate_zero(general, MAX_INTERRUPT_VECTORS * sizeof(thunk));
+    int_general = heap_locked(kh);
+    handlers = allocate_zero(int_general, MAX_INTERRUPT_VECTORS * sizeof(handlers[0]));
     assert(handlers != INVALID_ADDRESS);
-    interrupt_vector_heap = create_id_heap(general, general, INTERRUPT_VECTOR_START,
+    for (int i = 0; i < MAX_INTERRUPT_VECTORS; i++)
+        list_init(&handlers[i]);
+
+    interrupt_vector_heap = create_id_heap(int_general, int_general, INTERRUPT_VECTOR_START,
                                            MAX_INTERRUPT_VECTORS - INTERRUPT_VECTOR_START, 1, false);
     assert(interrupt_vector_heap != INVALID_ADDRESS);
 
@@ -279,7 +299,7 @@ void init_interrupts(kernel_heaps kh)
     /* initialize interrupt controller */
     init_gic();
 
-    /* timer init is minimal, stash irq bits here */
+    /* timer init is minimal, stash irq setup here */
     gic_set_int_config(GIC_TIMER_IRQ, GICD_ICFGR_LEVEL);
     gic_set_int_priority(GIC_TIMER_IRQ, 0);
     gic_set_int_target(GIC_TIMER_IRQ, 1);
