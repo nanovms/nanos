a71d413d51a6f49b21d875abb58e1207d8d4f273
Francesco Lavra <francescolavra.fl@gmail.com>
2022-01-28 12:59:12 +0100

    netconsole_write(): check if lwIP mutex is acquired
    Messages sento to the console may come from lwIP callbacks (e.g.
    when an IP address is acquired via DHCP). In these cases, the lwIP
    lock is already held by the current context, and thus trying to
    lock it leads to a "lock already held" error.
    This change defines a `mutex_is_acquired()` macro that returns
    whether a given mutex is held by the current context; this macro is
    used in netconsole_write() in order to lock the lwIP mutex only if
    it is not already held by the current context.

--- src/kernel/mutex.h
+++ src/kernel/mutex.h
@@ -10,4 +10,6 @@ void mutex_lock(mutex ql);
 
 void mutex_unlock(mutex ql);
 
+#define mutex_is_acquired(m)    ((m)->turn == get_current_context(current_cpu()))
+
 mutex allocate_mutex(heap h, u64 depth, u64 spin_iterations);
