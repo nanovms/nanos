8791ae99db74573589325396353555fa51dfd60b
Justin Sanders <sanderssj@gmail.com>
2022-02-21 22:37:26 -0500

    Add sigoverflow test This user test generates a signal handling loop that will fill up the stack and should generate a segfault. Since the segfault cannot be handled by the user program, the manifest specifies an expected exit code of 11.

---
+++ test/runtime/sigoverflow.c
@@ -0,0 +1,42 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <assert.h>
+#include <pthread.h>
+
+void handler(int signo)
+{
+    switch (signo) {
+    case SIGUSR1:
+        break;
+    case SIGABRT:
+        break;
+    default:
+        break;
+    }
+    abort();
+}
+
+void *thread(void *a)
+{
+    usleep(1000*1000);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    pthread_t pt;
+
+    setbuf(stdout, NULL);
+    printf("expecting signal stack overflow to segfault...\n");
+    pid_t pid = getpid();
+    pthread_create(&pt, NULL, thread, 0);
+    assert(signal(SIGUSR1, handler) != SIG_ERR);
+    assert(signal(SIGABRT, handler) != SIG_ERR);
+    kill(pid, SIGUSR1);
+    pthread_join(pt, 0);
+    return 0;
+}
+
