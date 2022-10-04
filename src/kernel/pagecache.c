nanovms/nanos

commit 68a8a3ca75ae0d7c5eead414dca2de994c6c4d30

pagecache: fix committing of nodes with empty `dirty` rangemap
pagecache_commit_dirty_node() copies any dirty ranges in a node to
a buffer and removes them from the `dirty` rangemap; then, it
triggers the actual committing of the dirty ranges. If
pagecache_commit_dirty_node() is called when the rangemap is empty,
the completion closure is invoked immediately even if the
committing is ongoing, which is incorrect and can cause issues such
as premature deallocation of fsfiles that have not been
synchronized to disk.
In addition, the buffer is not properly deallocated when the
rangemap is empty.
This commit fixes the above issues.

src/kernel/pagecache.c
@@ -937,23 +937,27 @@ static void pagecache_commit_dirty_node(pagecache_node pn, status_handler comple
     buffer b = allocate_buffer(h, sizeof(range));
     assert(b != INVALID_ADDRESS);
     if (rangemap_range_lookup(&pn->dirty, irange(0, infinity), stack_closure(dirty_range_handler, &pn->dirty, b)) != RM_MATCH) {
-        pagecache_unlock_node(pn);
-        if (complete)
-            apply(complete, STATUS_OK);
-        return;
+        deallocate_buffer(b);
+        b = 0;
     }
     pagecache_lock_volume(pn->pv);
     if (list_inserted(&pn->l))
         list_delete(&pn->l);
     pagecache_unlock_volume(pn->pv);
-    status_handler sh = closure(h, pagecache_commit_dirty_ranges, pn, b, complete);
+    status_handler sh;
+    if (b) {
+        sh = closure(h, pagecache_commit_dirty_ranges, pn, b, complete);
+        assert(sh != INVALID_ADDRESS);
+    } else  {
+        sh = complete;
+    }
     boolean busy = pn->committing;
     if (busy)
         assert(enqueue(pn->dirty_commits, sh));
     else
         pn->committing = true;
     pagecache_unlock_node(pn);
-    if (!busy)
+    if (!busy && sh)
         apply(sh, STATUS_OK);
 }
 

commit 6aeca5cf995851af50c7639ebac5f22078c1d268

pagecache_commit_dirty_node(): fix setting of `committing` flag
If pagecache_commit_dirty_node() is called on a node whose `dirty`
rangemap is empty and when no committing is ongoing, it is
incorrectly setting the `committing` flag to true. This prevents
any future calls to this function on a given node from invoking
their completion closure, which may cause syscalls such as fsync
and fdatasync to fail to return to userspace.
This cange fixes the above issue (introduced by commit
68a8a3ca75ae0d7c5eead414dca2de994c6c4d30).

src/kernel/pagecache.c
@@ -965,7 +965,7 @@ static void pagecache_commit_dirty_node(pagecache_node pn, status_handler comple
     boolean busy = pn->committing;
     if (busy)
         assert(enqueue(pn->dirty_commits, sh));
-    else
+    else if (b)
         pn->committing = true;
     pagecache_unlock_node(pn);
     if (!busy && sh)