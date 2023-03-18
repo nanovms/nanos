48b167a9419a0f47302b3c2c1612b71db31e3a6a
Justin Sanders <sanderssj@gmail.com>
2023-03-18 20:00:52 +0000

    Upgrade web.c to use http listener
    This modifies web.c and the user socket code to work with the
    http_listener code, which allows easier and better request handling
    and http support.

--- test/runtime/web.c
+++ test/runtime/web.c
@@ -3,21 +3,13 @@
 #include <socket_user.h>
 #include <sys/epoll.h>
 
-closure_function(2, 1, void, each_request,
-                 heap, h, buffer_handler, out,
-                 value, v)
+closure_function(1, 3, void, each_request,
+                 heap, h,
+                 http_method, m, buffer_handler, out, value, v)
 {
-    send_http_response(bound(out),
+    send_http_response(out,
                        timm("ContentType", "text/html"),
                        aprintf(bound(h), "unibooty!"));
-}
-
-closure_function(1, 1, buffer_handler, conn,
-                 heap, h,
-                 buffer_handler, out)
-{
-    heap h = bound(h);
-    return allocate_http_parser(h, closure(h, each_request, h, out));
 }
 
 // no good place to put this
@@ -29,9 +21,11 @@ int main(int argc, char **argv)
     tuple t = parse_arguments(h, argc, argv);
     notifier n = get(t, sym(select)) ? create_select_notifier(h) :
         get(t, sym(poll)) ? create_poll_notifier(h) :
-	create_epoll_notifier(h);
+        create_epoll_notifier(h);
     u16 port = 8080;
-    listen_port(h, n, port, closure(h, conn, h));
+    http_listener hl = allocate_http_listener(h, port);
+    http_register_default_handler(hl, closure(h, each_request, h));
+    listen_port(h, n, port, connection_handler_from_http_listener(hl));
     rprintf("Server started on port %d\n", port);
     notifier_spin(n);
     return 0;
