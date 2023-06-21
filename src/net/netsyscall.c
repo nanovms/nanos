nanovms/nanos

commit 0e67b81ef8406d15d5eec0f6eafdda95d0f9cba8

Network sockets: add missing decrement of tcp_pcb reference count
The socket_write_tcp_bh() and socket_close() functions were failing
to decrement the reference count of the tcp_pcb struct associated
with a TCP socket; this was preventing such structures from being
deallocated when a TCP connection is closed.

src/net/netsyscall.c
@@ -683,6 +683,7 @@ closure_function(6, 1, sysreturn, socket_write_tcp_bh,
                 rv = -EAGAIN;
                 goto out;
             }
+            tcp_unref(tcp_lw);
             net_debug(" send buf full, sleep\n");
             return blockq_block_required((unix_context)ctx, bqflags);
         }
@@ -755,6 +756,7 @@ closure_function(6, 1, sysreturn, socket_write_tcp_bh,
   out_unlock:
     netsock_unlock(s);
   out:
+    tcp_unref(tcp_lw);
     closure_finish();
     net_debug("   completion %p, rv %ld\n", completion, rv);
     apply(completion, rv);
@@ -1124,6 +1126,7 @@ closure_func_basic(fdesc_close, sysreturn, socket_close,
             tcp_close(tcp_lw);
             tcp_arg(tcp_lw, 0);
             netsock_tcp_put(tcp_lw);
+            tcp_unref(tcp_lw);
             netsock_check_loop();
         }
         break;

commit 09a83dd8db4a71c70a4168a5759dcc42a48582ce

socket_write_tcp_bh(): fix use of uninitialized variable tcp_lw
This change fixes a regression introduced in
0e67b81ef8406d15d5eec0f6eafdda95d0f9cba8, which causes tcp_unref()
to be called with an uninitialized argument if the `out_unlock`
goto target is reached.

src/net/netsyscall.c
@@ -677,13 +677,13 @@ closure_function(6, 1, sysreturn, socket_write_tcp_bh,
         if (avail == 0) {
           full:
             tcp_unlock(tcp_lw);
+            tcp_unref(tcp_lw);
             if ((bqflags & BLOCKQ_ACTION_BLOCKED) == 0 &&
                 ((s->sock.f.flags & SOCK_NONBLOCK) || (flags & MSG_DONTWAIT))) {
                 net_debug(" send buf full and non-blocking, return EAGAIN\n");
                 rv = -EAGAIN;
                 goto out;
             }
-            tcp_unref(tcp_lw);
             net_debug(" send buf full, sleep\n");
             return blockq_block_required((unix_context)ctx, bqflags);
         }
@@ -752,11 +752,11 @@ closure_function(6, 1, sysreturn, socket_write_tcp_bh,
         }
     }
     tcp_unlock(tcp_lw);
+    tcp_unref(tcp_lw);
     goto out;
   out_unlock:
     netsock_unlock(s);
   out:
-    tcp_unref(tcp_lw);
     closure_finish();
     net_debug("   completion %p, rv %ld\n", completion, rv);
     apply(completion, rv);