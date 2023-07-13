nanovms/nanos

commit 3eb8533b00aa77066d86f644a135b46552707e70

virtio_net: use minimal backing page sizes for caches
Given that there is limited queueing within the network stack, we can
reasonably predict that only up to virtqueue_entries(vn->rxq) rx buffers will
need to be cached - and similarly for tx handler completions. This adjusts the
objcache parent pagesizes to accommodate only these requirements, regardless
of whether the machine is in a 'low memory' configuration.

src/virtio/virtio_net.c
@@ -79,7 +79,7 @@ typedef struct xpbuf
     struct pbuf_custom p;
     vnet vn;
     closure_struct(vnet_input, input);
-} *xpbuf;
+} __attribute__((aligned(8))) *xpbuf;
 
 
 closure_function(1, 1, void, tx_complete,
@@ -277,6 +277,12 @@ static err_t virtioif_init(struct netif *netif)
     return ERR_OK;
 }
 
+static inline u64 find_page_size(bytes each, int n)
+{
+    /* extra element to cover objcache meta */
+    return 1ul << find_order(each * (n + 1));
+}
+
 static void virtio_net_attach(vtdev dev)
 {
     //u32 badness = VIRTIO_F_BAD_FEATURE | VIRTIO_NET_F_CSUM | VIRTIO_NET_F_GUEST_CSUM |
@@ -294,11 +300,6 @@ static void virtio_net_attach(vtdev dev)
         sizeof(struct virtio_net_hdr_mrg_rxbuf) : sizeof(struct virtio_net_hdr);
     vn->rxbuflen = pad(vn->net_header_len + sizeof(struct eth_hdr) + sizeof(struct eth_vlan_hdr) +
                        1500, 8);    /* padding to make xpbuf structures aligned to 8 bytes */
-    virtio_net_debug("%s: net_header_len %d, rxbuflen %d\n", __func__, vn->net_header_len, vn->rxbuflen);
-    vn->rxbuffers = allocate_objcache(h, (heap)contiguous, vn->rxbuflen + sizeof(struct xpbuf),
-                                      PAGESIZE_2M, true);
-    vn->txhandlers = allocate_objcache(h, (heap)contiguous, sizeof(closure_struct_type(tx_complete)),
-                                      PAGESIZE, true);
     mm_register_mem_cleaner(init_closure(&vn->mem_cleaner, vnet_mem_cleaner));
     /* rx = 0, tx = 1, ctl = 2 by 
        page 53 of http://docs.oasis-open.org/virtio/virtio/v1.0/cs01/virtio-v1.0-cs01.pdf */
@@ -306,12 +307,22 @@ static void virtio_net_attach(vtdev dev)
     virtio_alloc_virtqueue(dev, "virtio net tx", 1, &vn->txq);
     virtqueue_set_polling(vn->txq, true);
     virtio_alloc_virtqueue(dev, "virtio net rx", 0, &vn->rxq);
-    // just need vn->net_header_len contig bytes really
+    bytes rx_allocsize = vn->rxbuflen + sizeof(struct xpbuf);
+    bytes rxbuffers_pagesize = find_page_size(rx_allocsize, virtqueue_entries(vn->rxq));
+    bytes tx_handler_size = sizeof(closure_struct_type(tx_complete));
+    bytes tx_handler_pagesize = find_page_size(tx_handler_size, virtqueue_entries(vn->txq));
+    virtio_net_debug("%s: net_header_len %d, rx_allocsize %d, rxbuffers_pagesize %d "
+                     "tx_handler_size %d tx_handler_pagesize %d\n", __func__, vn->net_header_len,
+                     rx_allocsize, rxbuffers_pagesize, tx_handler_size, tx_handler_pagesize);
+    vn->rxbuffers = allocate_objcache(h, (heap)contiguous, rx_allocsize, rxbuffers_pagesize, true);
+    assert(vn->rxbuffers != INVALID_ADDRESS);
+    vn->txhandlers = allocate_objcache(h, (heap)contiguous, tx_handler_size, tx_handler_pagesize, true);
+    assert(vn->txhandlers != INVALID_ADDRESS);
     vn->empty = alloc_map(contiguous, contiguous->h.pagesize, &vn->empty_phys);
     assert(vn->empty != INVALID_ADDRESS);
-    for (int i = 0; i < vn->net_header_len; i++)  ((u8 *)vn->empty)[i] = 0;
+    for (int i = 0; i < vn->net_header_len; i++)
+        ((u8 *)vn->empty)[i] = 0;
     vn->n->state = vn;
-    // initialization complete
     vtdev_set_status(dev, VIRTIO_CONFIG_STATUS_DRIVER_OK);
     netif_add(vn->n,
               0, 0, 0, 

commit 091c5d0c9349937604874d173853af7b5a826c49

virtio-net: cap objcache pagesizes at PAGESIZE_2M
Commit 3eb8533b introduced a bug whereby large virtio-net queue
sizes (e.g. 4096 on gce) would result in an objcache pagesize being greater
than the maximum guaranteed alignment of the parent (linear backed) heap. This
could result in an assertion failure in the objcache code when allocating a
parent page that is not aligned to the objcache pagesize.

While the id heap aligns allocations to the rounded power-of-2 of the
allocation size (a property that ought to be parameterized...), the physical
id heap ranges themselves are only guaranteed to be aligned to 2M. Thus,
allocations from the physical id heap or linear backed heap may also only be
aligned up to a size of 2M.

The problem is avoided in this case by capping the objcache pagesize at
PAGESIZE_2M when setting up the virtio-net caches; a larger pagesize is
unnecessary anyhow, as the objcache meta accounts for less than .1% of a
PAGESIZE_2M.

src/virtio/virtio_net.c
@@ -280,7 +280,7 @@ static err_t virtioif_init(struct netif *netif)
 static inline u64 find_page_size(bytes each, int n)
 {
     /* extra element to cover objcache meta */
-    return 1ul << find_order(each * (n + 1));
+    return MIN(1ul << find_order(each * (n + 1)), PAGESIZE_2M);
 }
 
 static void virtio_net_attach(vtdev dev)
@@ -307,6 +307,8 @@ static void virtio_net_attach(vtdev dev)
     virtio_alloc_virtqueue(dev, "virtio net tx", 1, &vn->txq);
     virtqueue_set_polling(vn->txq, true);
     virtio_alloc_virtqueue(dev, "virtio net rx", 0, &vn->rxq);
+    virtio_net_debug("%s: rx q entries %d, tx q entries %d\n", __func__,
+                     virtqueue_entries(vn->rxq), virtqueue_entries(vn->txq));
     bytes rx_allocsize = vn->rxbuflen + sizeof(struct xpbuf);
     bytes rxbuffers_pagesize = find_page_size(rx_allocsize, virtqueue_entries(vn->rxq));
     bytes tx_handler_size = sizeof(closure_struct_type(tx_complete));