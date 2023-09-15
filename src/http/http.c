nanovms/nanos

commit 5301c6a793f1612525eeeef23926d77985d6e042

add vector value and tag type
This introduces a new vector type to the tagged value system. Previously,
vectors were represented as tuples with vector indices encoded as keys. With
these changes, a tagged vector, allocated via allocate_tagged_vector(), may be
accessed and manipulated using the standard vector functions found in
src/runtime/vector.h. As such, vector elements may be consumed, popped,
deleted, etc., whereas the previous treatment of vectors as tuples would make
such operations cumbersome, requiring an element to be deleted and re-inserted
if its index would change after an operation.

Note that these changes require a change to the tuple encoding format: an
additional bit has been added to the encoding type, allowing for a total of
four encoding types.

Also note that such changes would require a corresponding change to the
filesystem writing code in ops. It may be worth incrementing TFS_VERSION in
tfs_internal.h and allowing old encodings to be decoded (and types converted
to the new arrangement) when older versions are found.

src/http/http.c
@@ -175,20 +175,15 @@ static void reset_parser(http_parser p)
     p->state = STATE_INIT;
     p->header = allocate_tuple();
     p->word = allocate_buffer(p->h, 10);
-    p->start_line = allocate_vector(p->h, 3);
+    p->start_line = allocate_tagged_vector(3);
     p->content_length = 0;
 }
 
 static void cleanup_parser(http_parser p)
 {
-    buffer word;
-    vector_foreach(p->start_line, word) {
-        deallocate_buffer(word);
-    }
-    deallocate_vector(p->start_line);
     if (p->word != INVALID_ADDRESS)
         deallocate_buffer(p->word);
-    destruct_tuple(p->header, true);
+    destruct_value(p->header, true);
 }
 
 static void deallocate_parser(http_parser p)
@@ -206,7 +201,6 @@ closure_function(1, 1, status, http_recv,
                  buffer, b)
 {
     http_parser p = bound(p);
-    tuple start_line;
 
     /* content may be delimited by close rather than content length */
     if (!b) {
@@ -289,14 +283,7 @@ closure_function(1, 1, status, http_recv,
         return STATUS_OK;
 
   content_finish:
-    // XXX change from vector to tuple
-    start_line = allocate_tuple();
-    for (u64 i = 0; i < vector_length(p->start_line); i++) {
-        buffer a = vector_get(p->start_line, i);
-        set(start_line, intern_u64(i), a);
-    }
-    set(p->header, sym(start_line), start_line);
-    vector_clear(p->start_line);
+    set(p->header, sym(start_line), p->start_line);
     set(p->header, sym(content), p->word);
     p->word = INVALID_ADDRESS;
     apply(p->each, p->header);
@@ -399,7 +386,7 @@ closure_function(2, 1, void, each_http_request,
     http_method method;
     http_listener hl = bound(hl);
     http_responder hr = &bound(hr);
-    vector vsl = vector_from_tuple(hl->h, get(v, sym(start_line)));
+    vector vsl = get_vector(v, sym(start_line));
     if (!vsl || vsl == INVALID_ADDRESS)
         goto not_found;
 

klib/aws.c
@@ -17,7 +17,7 @@ closure_function(2, 1, void, aws_metadata_recv,
                  buffer_handler, handler, boolean *, done,
                  value, v)
 {
-    tuple resp = get_tuple(v, sym(start_line));
+    value resp = get(v, sym(start_line));
     buffer content = 0;
     if (resp) {
         buffer word;

klib/cloud_init.c
@@ -80,15 +80,13 @@ static enum cloud cloud_detect(void)
     return CLOUD_UNKNOWN;
 }
 
-static int cloud_init_parse_vector(tuple config, int (*parse_each)(tuple, vector), vector tasks)
+static int cloud_init_parse_vector(value config, int (*parse_each)(tuple, vector), vector tasks)
 {
-    if (!is_tuple(config))
+    if (!(is_tuple(config) || is_vector(config)))
         return KLIB_INIT_FAILED;
-    int num_entries;
-    for (num_entries = 0; get(config, intern_u64(num_entries)); num_entries++)
-        ;
-    for (int i = 0; i < num_entries; i++) {
-        value v = get(config, intern_u64(i));
+    /* allow parsing either tuple or vector for backward compatibility with older ops/tfs... */
+    value v;
+    for (int i = 0; (v = get(config, intern_u64(i))); i++) {
         if (!is_tuple(v))
             return KLIB_INIT_FAILED;
         int ret = parse_each(v, tasks);
@@ -190,7 +188,7 @@ closure_function(4, 1, void, cloud_download_save,
     status s;
     buffer content;
     if (*bound(received) == 0) {
-        tuple start_line = get_tuple(v, sym(start_line));
+        value start_line = get(v, sym(start_line));
         buffer status_code = get(start_line, sym(1));
         if (!status_code || (buffer_length(status_code) < 1) || (byte(status_code, 0) != '2')) {
             /* HTTP status code 2xx not found. */
@@ -535,7 +533,7 @@ closure_function(2, 1, void, cloud_download_env_set,
     if (!iterate(env, stack_closure(cloud_download_env_each, get_environment())))
         *bound(result) = timm("result", "failed to set environment variables");
   out:
-    destruct_tuple(v, true);
+    destruct_value(v, true);
 }
 
 closure_function(1, 1, void, cloud_download_env_err,
@@ -551,7 +549,7 @@ define_closure_function(1, 1, void, cloud_download_setenv,
                         value, v)
 {
     cloud_download_env cfg = struct_from_field(closure_self(), cloud_download_env, setenv);
-    tuple start_line = get_tuple(v, sym(start_line));
+    value start_line = get(v, sym(start_line));
     buffer status_code = get(start_line, sym(1));
     if (!status_code || (buffer_length(status_code) < 1) || (byte(status_code, 0) != '2')) {
         /* HTTP status code 2xx not found */
@@ -659,7 +657,7 @@ int init(status_handler complete)
     vector tasks = allocate_vector(cloud_heap, 8);
     assert(tasks != INVALID_ADDRESS);
     int ret;
-    tuple download = get(config, sym(download));
+    value download = get(config, sym(download));
     if (download) {
         ret = cloud_init_parse_vector(download, cloud_download_file_parse, tasks);
         if (ret != KLIB_INIT_OK) {
@@ -667,7 +665,7 @@ int init(status_handler complete)
             goto error;
         }
     }
-    tuple download_env = get(config, sym(download_env));
+    value download_env = get(config, sym(download_env));
     if (download_env) {
         ret = cloud_init_parse_vector(download_env, cloud_download_env_parse, tasks);
         if (ret != KLIB_INIT_OK) {

klib/cloudwatch.c
@@ -199,7 +199,7 @@ closure_function(1, 1, void, cw_logstream_vh,
                  value, v)
 {
     bound(parsed) = true;
-    tuple resp = get_tuple(v, sym(start_line));
+    value resp = get(v, sym(start_line));
     if (resp) {
         buffer word;
         for (u64 i = 0; (word = get(resp, intern_u64(i))); i++)
@@ -325,7 +325,7 @@ closure_function(1, 1, void, cw_loggroup_vh,
                  value, v)
 {
     bound(parsed) = true;
-    tuple resp = get_tuple(v, sym(start_line));
+    value resp = get(v, sym(start_line));
     if (resp) {
         buffer word;
         for (u64 i = 0; (word = get(resp, intern_u64(i))); i++)

klib/firewall.c
@@ -552,10 +552,10 @@ int init(status_handler complete)
         rprintf("invalid firewall configuration\n");
         return KLIB_INIT_FAILED;
     }
-    tuple rules = get(config, sym(rules));
+    value rules = get(config, sym(rules));
     if (!rules)
         return KLIB_INIT_OK;
-    if (!is_tuple(rules)) {
+    if (!(is_tuple(rules) || is_vector(rules))) {
         rprintf("invalid firewall rules\n");
         return KLIB_INIT_FAILED;
     }

klib/gcp.c
@@ -180,7 +180,7 @@ closure_function(1, 1, void, gcp_access_token_vh,
                  status_handler, sh,
                  value, v)
 {
-    tuple resp = get_tuple(v, sym(start_line));
+    value resp = get(v, sym(start_line));
     buffer status_code = get(resp, intern_u64(1));
     buffer content = get(v, sym(content));
     status s;
@@ -516,7 +516,7 @@ static void gcp_log_pending_delete(void)
 define_closure_function(0, 1, void, gcp_log_vh,
                         value, v)
 {
-    tuple resp = get_tuple(v, sym(start_line));
+    value resp = get(v, sym(start_line));
     buffer status_code = get(resp, intern_u64(1));
     if (status_code) {
         if (!buffer_strcmp(status_code, "200")) {
@@ -789,7 +789,7 @@ define_closure_function(0, 1, boolean, gcp_metrics_in_handler,
 define_closure_function(0, 1, void, gcp_metrics_value_handler,
                         value, v)
 {
-    tuple resp = get_tuple(v, sym(start_line));
+    value resp = get(v, sym(start_line));
     buffer status_code = get(resp, intern_u64(1));
     if (status_code) {
         if (!buffer_strcmp(status_code, "401")) {

klib/ntp.c
@@ -823,10 +823,11 @@ int init(status_handler complete)
         return KLIB_INIT_FAILED;
     }
     heap h = heap_locked(get_kernel_heaps());
-    tuple servers = get_tuple(root, sym(ntp_servers));
+    value servers = get(root, sym(ntp_servers));
     int server_count;
     if (servers) {
-        server_count = tuple_count(servers);
+        server_count = is_tuple(servers) ? tuple_count(servers) :
+            is_vector(servers) ? vector_length(servers) : 0;
         if (server_count == 0) {
             servers = 0;
             server_count = 1;

klib/radar.c
@@ -246,8 +246,8 @@ closure_function(0, 1, void, telemetry_crash_recv,
                  value, v)
 {
     if (v) {
-        tuple resp = get(v, sym(start_line));
-        if (resp && is_tuple(resp)) {
+        value resp = get(v, sym(start_line));
+        if (resp) {
             buffer word;
             for (u64 i = 0; (word = get(resp, intern_u64(i))); i++)
                 if (buffer_strstr(word, "OK") == 0) {

commit 5e42a18ded5d767f46bfb6e9f070f302a2e12b73

fix double free in each_http_request()
Commit 5301c6a7 introduced a regression whereby the "start_line" vector
attribute of an http request would be explicitly freed, which would then later
cause a double free during parser cleanup. This change corrects this issue by
treating the vector as an immutable value (whereas previously it was cloned
and thus consumable).

src/http/http.c
@@ -392,7 +392,7 @@ closure_function(2, 1, void, each_http_request,
 
     buffer ver = vector_get(vsl, 2);
     if (ver) {
-        get_http_ver(ver, &hr->http_version);
+        get_http_ver(alloca_wrap(ver), &hr->http_version);
         if (hr->http_version > HTTP_VER(1, 1))
             goto bad_ver;
 
@@ -418,7 +418,7 @@ closure_function(2, 1, void, each_http_request,
         if (!hl->default_handler)
             goto not_found;
         apply(hl->default_handler, method, hr, v);
-        goto out;
+        return;
     }
 
     buffer rel_uri = clone_buffer(hl->h, uri);
@@ -453,19 +453,13 @@ closure_function(2, 1, void, each_http_request,
 
     if (match)
         apply(match->each, method, hr, v);
-out:
-    deallocate_vector(vsl);
     return;
   not_found:
-    if (vsl != INVALID_ADDRESS)
-        deallocate_vector(vsl);
     send_http_response(hr, timm("status", "404 Not Found"),
                        aprintf(hl->h, "<html><head><title>404 Not Found</title></head>"
                                "<body><h1>Not Found</h1></body></html>\r\n"));
     return;
   bad_ver:
-    if (vsl != INVALID_ADDRESS)
-        deallocate_vector(vsl);
     send_http_response(hr, timm("status", "505 HTTP Version Not Supported"),
                        aprintf(hl->h, "<html><head><title>505 HTTP Version Not Supported</title></head>"
                                "<body><h1>Use HTTP/1.1</h1></body></html>\r\n"));