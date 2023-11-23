nanovms/nanos

commit d1515087cec48ea8ccc4de0c504544a73393d11b

Kernel heaps: add DMA memory heap
Currently, the kernel maps all pysical memory linearly and uses
this mapping for both kernel-internal data structures and DMA
transfers.
This change creates an additional kernel heap specifically for DMA
operation; this will allow the kernel to run on machines where
generic kernel memory cannot be used for DMA (e.g. when using
memory encryption). The linear backed heap is now used for DMA
memory, whereas a second linear mapping is used for (generic)
kernel data memory; this second mapping, and the corresponding
duplication of heaps, is only done when actually needed by the
platform, otherwise the DMA heap is the same as the generic heap.
When kernel data has to be transferred via DMA on a platform where
DMA memory is different from kernel memory, the transfer is done
using bounce buffering.
Page table initialization has been consolidated for all supported
platforms, and now the init_page_tables() function is called from
common code in init_kernel_heaps().The page table code has been
revised to be able to used a non-hardcoded linear mapping.
The bootstrap heap is now used after initialization as backing
heap for the virtual_page heap.
Heap usage throguhout the kernel has been revised so that all
non-DMA memory is allocated from the page_backed heap (either
directly or via the general and locked heaps), and all DMA memory
is allocated from the linear_backed heap (either directly, or via
the DMA heap).

src/fs/tlog.c
@@ -62,7 +62,7 @@ declare_closure_struct(1, 0, void, log_ext_free,
 
 struct log_ext {
     log tl;
-    buffer staging;
+    struct buffer staging;
     boolean open;
     boolean old_encoding;
 
@@ -122,9 +122,9 @@ define_closure_function(1, 0, void, log_ext_free,
                         log_ext, ext)
 {
     log_ext ext = bound(ext);
-    if (ext->staging)
-        deallocate_buffer(ext->staging);
-    heap h = ext->tl->h;
+    log tl = ext->tl;
+    deallocate(tl->fs->dma, ext->staging.contents, ext->staging.length);
+    heap h = tl->h;
     deallocate(h, ext, sizeof(struct log_ext));
 }
 
@@ -135,13 +135,16 @@ static log_ext open_log_extension(log tl, range sectors)
     if (ext == INVALID_ADDRESS)
         return ext;
     ext->tl = tl;
-    ext->staging = allocate_buffer(tl->h, size_bytes);
-    if (ext->staging == INVALID_ADDRESS)
+    tfs fs = tl->fs;
+    heap dma = fs->dma;
+    void *staging = allocate(dma, size_bytes);
+    if (staging == INVALID_ADDRESS)
         goto fail_dealloc;
+    init_buffer(&ext->staging, size_bytes, true, 0, staging);
     ext->open = false;
     ext->old_encoding = false;
-    init_closure(&ext->read, log_storage_op, tl->fs, sectors.start, false);
-    init_closure(&ext->write, log_storage_op, tl->fs, sectors.start, true);
+    init_closure(&ext->read, log_storage_op, fs, sectors.start, false);
+    init_closure(&ext->write, log_storage_op, fs, sectors.start, true);
     ext->sectors = sectors;
     init_refcount(&ext->refcount, 1, init_closure(&ext->free, log_ext_free, ext));
 #ifndef TLOG_READ_ONLY
@@ -158,7 +161,7 @@ static log_ext open_log_extension(log tl, range sectors)
     return ext;
 #ifndef TLOG_READ_ONLY
   fail_dealloc_staging:
-    deallocate_buffer(ext->staging);
+    deallocate(dma, staging, size_bytes);
 #endif
   fail_dealloc:
     deallocate(tl->h, ext, sizeof(struct log_ext));
@@ -278,7 +281,7 @@ static void flush_log_extension(log_ext ext, boolean release, status_handler com
 {
     refcount_reserve(&ext->refcount);
     tfs fs = ext->tl->fs;
-    buffer b = ext->staging;
+    buffer b = &ext->staging;
     tlog_ext_lock(ext);
     dump_staging(ext);
     push_u8(b, END_OF_LOG);
@@ -325,13 +328,14 @@ static log_ext log_ext_new(log tl)
 static void log_extension_init(log_ext ext)
 {
     assert(!ext->open);
-    assert(push_buffer(ext->staging, alloca_wrap_buffer(tfs_magic, TFS_MAGIC_BYTES)));
-    push_varint(ext->staging, TFS_VERSION);
-    push_varint(ext->staging, range_span(ext->sectors));
+    buffer staging = &ext->staging;
+    assert(push_buffer(staging, alloca_wrap_buffer(tfs_magic, TFS_MAGIC_BYTES)));
+    push_varint(staging, TFS_VERSION);
+    push_varint(staging, range_span(ext->sectors));
     if (ext->sectors.start == 0) {
-        assert(buffer_write(ext->staging, ext->tl->fs->uuid, UUID_LEN));
-        assert(buffer_write_cstring(ext->staging, ext->tl->fs->label));
-        push_u8(ext->staging, '\0');   /* label string terminator */
+        assert(buffer_write(staging, ext->tl->fs->uuid, UUID_LEN));
+        assert(buffer_write_cstring(staging, ext->tl->fs->label));
+        push_u8(staging, '\0');   /* label string terminator */
     }
 }
 
@@ -350,7 +354,7 @@ closure_function(3, 1, void, log_extend_link,
 
     /* add link to close out old extension and commit */
     log_ext old_ext = bound(old_ext);
-    buffer b = old_ext->staging;
+    buffer b = &old_ext->staging;
     push_u8(b, LOG_EXTENSION_LINK);
     push_varint(b, bound(sectors).start);
     push_varint(b, range_span(bound(sectors)));
@@ -409,8 +413,9 @@ static inline boolean log_write_internal(log tl, merge m)
         do {
             assert(buffer_length(tl->tuple_staging) > 0);
             size = log_size(ext);
+            buffer staging = &ext->staging;
             u64 min = TFS_EXTENSION_LINK_BYTES + TUPLE_AVAILABLE_MIN_SIZE;
-            if (ext->staging->end + min >= size || ext->old_encoding) {
+            if (staging->end + min >= size || ext->old_encoding) {
                 tlog_ext_unlock(ext);
                 status_handler sh = apply_merge(m);
                 ext = log_extend(tl, TFS_LOG_DEFAULT_EXTENSION_SIZE, sh);
@@ -419,20 +424,20 @@ static inline boolean log_write_internal(log tl, merge m)
                 size = log_size(ext);
                 tlog_ext_lock(ext);
             }
-            assert(ext->staging->end + min < size);
-            u64 avail = size - (ext->staging->end + TFS_EXTENSION_LINK_BYTES + TUPLE_AVAILABLE_HEADER_SIZE);
+            assert(staging->end + min < size);
+            u64 avail = size - (staging->end + TFS_EXTENSION_LINK_BYTES + TUPLE_AVAILABLE_HEADER_SIZE);
             u64 length = MIN(avail, remaining);
             if (written == 0) {
-                push_u8(ext->staging, TUPLE_AVAILABLE);
-                push_varint(ext->staging, remaining);
+                push_u8(staging, TUPLE_AVAILABLE);
+                push_varint(staging, remaining);
             } else {
-                push_u8(ext->staging, TUPLE_EXTENDED);
+                push_u8(staging, TUPLE_EXTENDED);
             }
-            push_varint(ext->staging, length);
+            push_varint(staging, length);
 
             /* No graceful way to recover from an alloc failure here, and
                flushing could leave the log extension in a corrupt state. */
-            assert(buffer_write(ext->staging, buffer_ref(tl->tuple_staging, 0), length));
+            assert(buffer_write(staging, buffer_ref(tl->tuple_staging, 0), length));
             buffer_consume(tl->tuple_staging, length);
             remaining -= length;
             written += length;
@@ -720,7 +725,7 @@ closure_function(4, 1, void, log_read_complete,
     }
 
     /* staging is preallocated to size */
-    buffer b = ext->staging;
+    buffer b = &ext->staging;
     buffer_produce(b, bound(length));
     deallocate_sg_list(bound(sg));
     dump_staging(ext);
@@ -864,7 +869,7 @@ static void log_read(log tl, status_handler sh)
     range r = irangel(0, bytes_from_sectors(&tl->fs->fs, range_span(ext->sectors)));
     u64 length = range_span(r);
     sg_buf sgb = sg_list_tail_add(sg, length);
-    sgb->buf = buffer_ref(ext->staging, 0);
+    sgb->buf = buffer_ref(&ext->staging, 0);
     sgb->size = length;
     sgb->offset = 0;
     sgb->refcount = 0;

platform/pc/boot/stage2.c
@@ -80,6 +80,11 @@ void klog_write(const char *s, bytes count)
 {
 }
 
+heap heap_dma(void)
+{
+    return general;
+}
+
 closure_function(1, 1, void, stage2_bios_read,
                  u64, offset,
                  storage_req, req)

platform/pc/service.c
@@ -242,7 +242,6 @@ static void __attribute__((noinline)) init_service_new_stack()
 {
     kernel_heaps kh = get_kernel_heaps();
     early_init_debug("in init_service_new_stack");
-    init_page_tables((heap)heap_linear_backed(kh));
     bytes pagesize = is_low_memory_machine() ? PAGESIZE : PAGESIZE_2M;
     init_integers(locking_heap_wrapper(heap_general(kh),
                   allocate_tagged_region(kh, tag_integer, pagesize)));

platform/riscv-virt/service.c
@@ -68,6 +68,8 @@ void read_kernel_syms(void)
 
 void reclaim_regions(void)
 {
+    /* mmu init complete; unmap temporary identity map */
+    unmap(PHYSMEM_BASE, INIT_IDENTITY_SIZE);
 }
 
 static inline void virt_shutdown(u64 code)
@@ -100,9 +102,6 @@ static void __attribute__((noinline)) init_service_new_stack(void)
 {
     init_debug("in init_service_new_stack\n");
     kernel_heaps kh = get_kernel_heaps();
-    init_page_tables((heap)heap_linear_backed(kh));
-    /* mmu init complete; unmap temporary identity map */
-    unmap(PHYSMEM_BASE, INIT_IDENTITY_SIZE);
     bytes pagesize = is_low_memory_machine() ? PAGESIZE : PAGESIZE_2M;
     init_integers(locking_heap_wrapper(heap_general(kh),
                   allocate_tagged_region(kh, tag_integer, pagesize)));

platform/virt/service.c
@@ -247,7 +247,6 @@ static void __attribute__((noinline)) init_service_new_stack(void)
 {
     init_debug("in init_service_new_stack\n");
     kernel_heaps kh = get_kernel_heaps();
-    init_page_tables((heap)heap_linear_backed(kh));
     bytes pagesize = is_low_memory_machine() ? PAGESIZE : PAGESIZE_2M;
     init_integers(locking_heap_wrapper(heap_general(kh),
                   allocate_tagged_region(kh, tag_integer, pagesize)));

src/aarch64/gic.c
@@ -290,7 +290,7 @@ boolean dev_irq_enable(u32 dev_id, int vector)
             itt_size = MAX(itt_size, U64_FROM_BIT(8));  /* ensure 256-byte alignment */
             gic_debug("creating ITT with %ld entries (%ld bytes)\n", ite_num, itt_size);
             u64 pa;
-            dev->itt = alloc_map(heap_page_backed(kh), itt_size, &pa);
+            dev->itt = alloc_map(heap_linear_backed(kh), itt_size, &pa);
             if (dev->itt == INVALID_ADDRESS) {
                 deallocate(heap_locked(kh), dev, sizeof(*dev));
                 return false;
@@ -384,6 +384,7 @@ static void init_gits(kernel_heaps kh)
         gic.redist.rdbase = gic.redist.base >> 16;
     else
         gic.redist.rdbase = GICR_TYPER_PROC_NUM(gicr_read_64(TYPER));
+    backed_heap backed = heap_linear_backed(kh);
     u64 pa;
     for (int n = 0; n < 8; n++) {
         u64 base = gits_read_64(BASER(n));
@@ -410,7 +411,7 @@ static void init_gits(kernel_heaps kh)
                       GITS_TABLE_TYPE(base), GITS_TABLE_ENTRY_SIZE(base) + 1, page_size);
             if (GITS_TABLE_TYPE(base) == GITS_TABLE_DEVICES)
                 gic.dev_id_limit = page_size / (GITS_TABLE_ENTRY_SIZE(base) + 1);
-            table = alloc_map(heap_page_backed(kh), page_size, &pa);
+            table = alloc_map(backed, page_size, &pa);
             assert(table != INVALID_ADDRESS);
             zero(table, page_size);
             base = (base & ~GITS_BASE_PA_MASK) | pa;
@@ -421,7 +422,7 @@ static void init_gits(kernel_heaps kh)
     list_init(&gic.devices);
 
     /* Set up the command queue. */
-    gic.its_cmd_queue = alloc_map(heap_page_backed(kh), GIC_CMD_QUEUE_SIZE, &pa);
+    gic.its_cmd_queue = alloc_map(backed, GIC_CMD_QUEUE_SIZE, &pa);
     assert(gic.its_cmd_queue != INVALID_ADDRESS);
     gits_write_64(CBASER, GITS_CBASER_VALID | pa | (GIC_CMD_QUEUE_SIZE / PAGESIZE));
     gits_write_64(CWRITER, 0);
@@ -488,15 +489,16 @@ int init_gic(void)
         /* Set up a page-sized LPI configuration table. */
         gic_msi_vector_num = MAX(gic_msi_vector_num, PAGESIZE); /* 1 byte per LPI */
         kernel_heaps kh = get_kernel_heaps();
+        backed_heap backed = heap_linear_backed(kh);
         u64 pa;
-        gic.lpi_cfg_table = alloc_map(heap_page_backed(kh), PAGESIZE, &pa);
+        gic.lpi_cfg_table = alloc_map(backed, PAGESIZE, &pa);
         assert(gic.lpi_cfg_table != INVALID_ADDRESS);
         zero(gic.lpi_cfg_table, PAGESIZE);
         u64 id_bits = find_order(gic_msi_vector_base + gic_msi_vector_num) - 1;
         gicr_write_64(PROPBASER, pa | id_bits);
 
         /* Set up LPI pending table, which must be aligned to 64 KB. */
-        void *lpi_pending_table = alloc_map(heap_page_backed(kh), 64 * KB, &pa);
+        void *lpi_pending_table = alloc_map(backed, 64 * KB, &pa);
         assert(lpi_pending_table != INVALID_ADDRESS);
         zero(lpi_pending_table, 64 * KB);
         gicr_write_64(PENDBASER, GICR_PENDBASER_PTZ | pa);

src/aarch64/kernel_machine.c
@@ -138,7 +138,7 @@ static void ap_start(void)
 {
     int cpuid = cpuid_from_mpid(read_mpid());
     assert(cpuid >= 0);
-    cpuinfo ci = init_cpuinfo((heap)heap_linear_backed(get_kernel_heaps()), cpuid);
+    cpuinfo ci = init_cpuinfo(heap_locked(get_kernel_heaps()), cpuid);
     assert(ci != INVALID_ADDRESS);
     context_frame f = ci->m.kernel_context->frame;
     switch_stack_1(frame_get_stack_top(f), ap_start_newstack, cpuid);

src/aarch64/page_machine.h
@@ -232,6 +232,11 @@ static inline pageflags pageflags_device(void)
             | PAGE_DEFAULT_PERMISSIONS};
 }
 
+static inline pageflags pageflags_dma(void)
+{
+    return (pageflags){.w = pageflags_memory().w & ~PAGE_READONLY};
+}
+
 static inline pageflags pageflags_writable(pageflags flags)
 {
     return (pageflags){.w = flags.w & ~PAGE_READONLY};

commit e7f31649e2c5e672acdf0e6aca1d6f526e034fea

TFS log_write_internal(): fix writing to new log extension
When the current log extension of a TFS filesystem is full and a
new extension is created, any log entries that could not fit in the
current extension must be written to the staging buffer of the new
extension.
This change fixes a regression introduced in commit d151508. In
addition, it fixes a build failure when the TLOG_DEBUG_DUMP
preprocessor #define is present.

Closes #1970.

src/fs/tlog.c
@@ -249,7 +249,7 @@ static log log_new(heap h, tfs fs)
 static void dump_staging(log_ext ext)
 {
 #ifdef TLOG_DEBUG_DUMP
-    buffer b = ext->staging;
+    buffer b = &ext->staging;
     u64 z = b->end;
     rprintf("staging contains:\n");
     for (int i = 0; i < 4; i++) {
@@ -422,6 +422,7 @@ static inline boolean log_write_internal(log tl, merge m)
                 if (ext == INVALID_ADDRESS)
                     return false;
                 size = log_size(ext);
+                staging = &ext->staging;
                 tlog_ext_lock(ext);
             }
             assert(staging->end + min < size);