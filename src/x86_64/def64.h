e0814bc034e9acb8b8f7b07a1c97681d4176f360
Francesco Lavra <francescolavra.fl@gmail.com>
2024-03-08 09:03:01 +0100

    x86: move byte order conversion helpers from def64.h to machine.h
    This is being done so that macros such as htobe32() and le64toh()
    can be called from buffer.h (which will be needed in the next
    commit).

--- src/x86_64/def64.h
+++ src/x86_64/def64.h
@@ -55,64 +55,6 @@ typedef u8 value_tag;
         __x;\
      })
 
-/* These are defined as functions to avoid multiple evaluation of x. */
-static inline u16
-__bswap16(u16 _x)
-{
-    return (u16)(_x << 8 | _x >> 8);
-}
-
-static inline u32
-__bswap32(u32 _x)
-{
-    return ((u32)__bswap16(_x & 0xffff) << 16) | __bswap16(_x >> 16);
-}
-
-static inline u64
-__bswap64(u64 _x)
-{
-    return ((u64)__bswap32(_x & 0xffffffff) << 32) | __bswap32(_x >> 32);
-}
-
-#ifndef htobe16
-#define htobe16(x) __bswap16(x)
-#endif
-#ifndef be16toh
-#define be16toh(x) __bswap16(x)
-#endif
-#ifndef htole16
-#define htole16(x) (x)
-#endif
-#ifndef le16toh
-#define le16toh(x) (x)
-#endif
-
-#ifndef htobe32
-#define htobe32(x) __bswap32(x)
-#endif
-#ifndef be32toh
-#define be32toh(x) __bswap32(x)
-#endif
-#ifndef htole32
-#define htole32(x) (x)
-#endif
-#ifndef le32toh
-#define le32toh(x) (x)
-#endif
-
-#ifndef htobe64
-#define htobe64(x) __bswap64(x)
-#endif
-#ifndef be64toh
-#define be64toh(x) __bswap64(x)
-#endif
-#ifndef htole64
-#define htole64(x) (x)
-#endif
-#ifndef le64toh
-#define le64toh(x) (x)
-#endif
-
 /* returns -1 if x == 0, caller must check */
 static inline u64 msb(u64 x)
 {
