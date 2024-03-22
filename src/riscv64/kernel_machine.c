nanovms/nanos

commit 364b517d03a7296e8a5cb0326292d87efac44e07

Strings: remove unbounded memory access
This changeset removes the use of string functions that access
string memory without a limit (i.e. that rely on the presence of a
string terminator to determine the end of a string). Instead, a new
string type (sstring) is being defined; this type is a struct that
includes a pointer to string memory and the length of the string;
use of this type makes it unnecessary to look for the string
terminator in order to determine the length of a string, and thus
avoids unbounded access to string memory.
C string literals (which by definition are NULL-terminated strings)
are still allowed in the kernel code, but they are converted at
compile time into sstring types, e.g. by using the ss() macro.
Since it is still necessary for the kernel to be able to process
NULL-terminated strings (for example, strings read by drivers from
peripheral memory), the sstring_from_cstring() function is being
defined: this function takes a memory pointer and a maximum length
value, and returns an sstring built by parsing the memory (up to
the maximum length) looking for the string terminator (if no
terminator is found, the returned string length is the maximum
length).
The "%s" format specifier in printf-style functions now takes an
sstring argument instead of a `char *`; the name field has been
removed from closure structs, and the function handling the "%F"
format specifier has been changed to retrieve a closure name from
the kernel symbol table.

src/riscv64/kernel_machine.c
@@ -132,17 +132,17 @@ void start_secondary_cores(kernel_heaps kh)
 
 closure_function(1, 2, boolean, cpu_dtb_handler,
                  u64 *, proc_count,
-                 dt_node, n, char *, name)
+                 dt_node, n, sstring, name)
 {
-    dt_prop device_type = dtb_get_prop(DEVICETREE, n, "device_type");
+    dt_prop device_type = dtb_get_prop(DEVICETREE, n, ss("device_type"));
     if (device_type == INVALID_ADDRESS)
         return true;
     dt_value dtval = dtb_read_value(DEVICETREE, n, device_type);
     if (dtval.type != DT_VALUE_STRING ||
-        runtime_strcmp(dtval.u.string, "cpu"))
+        runtime_strcmp(isstring(dtval.u.string, dtval.dlen), ss("cpu")))
         return true;
 
-    dt_prop reg = dtb_get_prop(DEVICETREE, n, "reg");
+    dt_prop reg = dtb_get_prop(DEVICETREE, n, ss("reg"));
     if (reg == INVALID_ADDRESS)
         halt("unable to find \"reg\" property for cpu\n");
     dt_value reg_val = dtb_read_value(DEVICETREE, n, reg);
@@ -162,14 +162,15 @@ void count_cpus_present(void)
     hartids_by_cpuid = allocate_vector(heap_general(get_kernel_heaps()), 8);
     assert(hartids_by_cpuid != INVALID_ADDRESS);
     vector_set(hartids_by_cpuid, 0, (void*)boot_hartid);
-    dt_node n = dtb_find_node_by_path(DEVICETREE, "/cpus");
+    dt_node n = dtb_find_node_by_path(DEVICETREE, ss("/cpus"));
     if (n == INVALID_ADDRESS) {
         msg_err("unable to find \"/cpus/cpu-map/cluster0\" in device tree; resorting to single cpu\n");
         return;
     }
 
     u64 proc_count = 1;
-    dtb_walk_node_children(DEVICETREE, n, 0, stack_closure(cpu_dtb_handler, &proc_count));
+    dtb_walk_node_children(DEVICETREE, n, sstring_null(),
+                           stack_closure(cpu_dtb_handler, &proc_count));
     present_processors = proc_count;
 }
 

src/aarch64/kernel_machine.c
@@ -28,7 +28,7 @@ struct tagheap {
 static void tag_dealloc(heap h, u64 a, bytes s)
 {
     struct tagheap *th = (struct tagheap *)h;
-    tag_debug("%s: tag %d, a 0x%lx, s 0x%lx\n", __func__, th->vtag >> VA_TAG_OFFSET, a, s);
+    tag_debug("%s: tag %d, a 0x%lx, s 0x%lx\n", func_ss, th->vtag >> VA_TAG_OFFSET, a, s);
     deallocate_u64(th->mh, a & MASK(VA_TAG_OFFSET), s);
 }
 
@@ -41,7 +41,7 @@ static u64 tag_alloc(heap h, bytes s)
     u64 a = u64_from_pointer(p);
     assert((a >> VA_TAG_OFFSET) == 0);
     a |= th->vtag;
-    tag_debug("%s: tag %d, s 0x%lx, a 0x%lx\n", __func__, th->vtag >> VA_TAG_OFFSET, s, a);
+    tag_debug("%s: tag %d, s 0x%lx, a 0x%lx\n", func_ss, th->vtag >> VA_TAG_OFFSET, s, a);
     return a;
 }
 
@@ -61,7 +61,7 @@ heap allocate_tagged_region(kernel_heaps kh, u64 tag, bytes pagesize, boolean lo
     th->h.allocated = 0;
     th->h.total = 0;
     th->h.management = 0;
-    tag_debug("%s: tag %d, bits 0x%lx, heap %p\n", __func__, tag, th->vtag, th);
+    tag_debug("%s: tag %d, bits 0x%lx, heap %p\n", func_ss, tag, th->vtag, th);
     return &th->h;
 }
 

commit 439a4670468810942dd3002c1e13b3d82a412161

RISC-V SMP: fix parsing of CPU device tree entries
In the RISC-V SMP initialization code, the device tree is parsed in
order to retrieve the number of present CPUs. Commit
364b517d03a7296e8a5cb0326292d87efac44e07 introduced a regression by
which a cpu device tree entry, whose name is null-terminated in the
device tree, can no longer be recognized, thereby preventing the
kernel from enumerating the application processors.
This change fixes the above regression by using the
sstring_from_cstring() function when parsing a device tree entry
name.

src/riscv64/kernel_machine.c
@@ -139,7 +139,7 @@ closure_function(1, 2, boolean, cpu_dtb_handler,
         return true;
     dt_value dtval = dtb_read_value(DEVICETREE, n, device_type);
     if (dtval.type != DT_VALUE_STRING ||
-        runtime_strcmp(isstring(dtval.u.string, dtval.dlen), ss("cpu")))
+        runtime_strcmp(sstring_from_cstring(dtval.u.string, dtval.dlen), ss("cpu")))
         return true;
 
     dt_prop reg = dtb_get_prop(DEVICETREE, n, ss("reg"));