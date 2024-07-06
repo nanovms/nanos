1789b5580cd5e6caf47b87b41421b5689e84435f
Francesco Lavra <francescolavra.fl@gmail.com>
2024-07-06 18:46:28 +0200

    stage3 program_start(): fix remapping of read-only after init areas
    The size of memory areas to be remapped in read-only mode after
    initialization is calculated as "&*_end - &*_start"; if "*start"
    and "*end" are declared as pointers, the difference between their
    addresses, using pointer arithmetic, is equal to 1/8 (a pointer is
    64-bit wide) of the numeric difference between address values.
    This commit fixes the above issue by declaring "*start" and "*end"
    as byte-sized variables, so that the difference between their
    addresses equals the expected value.

--- src/kernel/stage3.c
+++ src/kernel/stage3.c
@@ -24,8 +24,8 @@ closure_function(3, 1, void, program_start,
 
     /* Set mapping flags to read-only for data that has been initialized during boot and should not
      * be modified afterwards. */
-    extern void *ro_after_init_start, *ro_after_init_end;
-    extern void *bss_ro_after_init_start, *bss_ro_after_init_end;
+    extern u8 ro_after_init_start, ro_after_init_end;
+    extern u8 bss_ro_after_init_start, bss_ro_after_init_end;
     update_map_flags(u64_from_pointer(&ro_after_init_start),
                      &ro_after_init_end - &ro_after_init_start,
                      pageflags_memory());
