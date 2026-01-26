622fe28666840b766ed280d56b56376043647fc5
Francesco Lavra <francescolavra@nanovms.com>
2026-01-26 09:13:31 +0100

    ntp: move div128_64() to non-klib code
    To implement the ntp_adjtime functionality, this function will be called
    also from core kernel code; move it to a new util.c file that is compiled
    in the core kernel binary.

---
+++ src/runtime/util.c
@@ -0,0 +1,22 @@
+#include "runtime.h"
+#include "util.h"
+
+/* Calculates a division between a 128-bit value and a 64-bit value and returns a 64-bit quotient.
+ * If the quotient does not fit in 64 bits, -1ull is returned.
+ * Only one 64-bit division is executed, thus the result may not be as accurate as it could be.
+ */
+u64 div128_64(u128 dividend, u64 divisor)
+{
+    if (dividend == 0)
+        return 0;
+    if (divisor == 0)
+        return -1ull;
+    u64 dividend_msb = (dividend >> 64) ? (64 + msb(dividend >> 64)) : msb(dividend);
+    if (dividend_msb <= 63)
+        return ((u64)dividend) / divisor;
+    u64 shift = dividend_msb - 63;
+    u64 div = ((u64)(dividend >> shift)) / divisor;
+    if (msb(div) >= 64 - shift)
+        return -1ull;
+    return (div << shift);
+}
