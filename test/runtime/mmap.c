4d61547175291c0ec6474ca40685b29cf1f84273
Francesco Lavra <francescolavra@nanovms.com>
2026-03-01 13:20:18 +0100

    mmap (exec protection): Enforce read-only access for executable mappings
    When exec protection is enabled, a mapping for an executable file must
    never be granted write access, even if it's a private mapping, otherwise a
    program can bypass exec protection by executing arbitrary code in memory.

--- test/runtime/mmap.c
+++ test/runtime/mmap.c
@@ -211,7 +211,6 @@ static void mmap_newfile_test(void)
 }
 
 /*
- * Try to mmap a non-executable file with exec access
  * Checks that mmap fails and sets errno to EACCES
  */
 static void check_exec_perm_test(void)
@@ -220,6 +219,7 @@ static void check_exec_perm_test(void)
     const size_t maplen = 1;
     void *addr;
 
+    /* Try to mmap a non-executable file with exec access. */
     fd = open("new_file_noexec", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
     if (fd < 0) {
         test_perror("new file open");
@@ -233,6 +233,13 @@ static void check_exec_perm_test(void)
     if (close(fd) < 0) {
         test_perror("new file close");
     }
+
+    /* Try to mmap an executable file with write access. */
+    fd = open("/proc/self/exe", O_RDONLY);
+    test_assert(fd >= 0);
+    addr = mmap(NULL, maplen, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+    test_assert((addr == MAP_FAILED) && (errno == EACCES));
+    close(fd);
 }
 
 /*
